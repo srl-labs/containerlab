ea16cc7e8ab9e3e7499df5427efb9c7ffedf4797
karimra <karim.radhouani@nokia.com>
2020-08-31 15:42:57 +0800

    add cLab struct

--- src/docker.go
+++ src/docker.go
@@ -6,7 +6,6 @@ import (
 	"os/exec"
 	"strings"
 
-	"docker.io/go-docker"
 	"docker.io/go-docker/api/types"
 	"docker.io/go-docker/api/types/container"
 	"docker.io/go-docker/api/types/network"
@@ -14,30 +13,15 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Docker struct
-type Docker struct {
-	cli *docker.Client
-}
-
-// NewDocker initializes the docker client
-func NewDocker() (d *Docker, err error) {
-	d = new(Docker)
-	d.cli, err = docker.NewEnvClient()
-	if err != nil {
-		return nil, err
-	}
-	return d, nil
-}
-
-func (d *Docker) createBridge() (err error) {
+func (c *cLab) createBridge() (err error) {
 
 	ipamIPv4Config := network.IPAMConfig{
-		Subnet:  DockerInfo.Ipv4Subnet,
-		Gateway: DockerInfo.Ipv4Gateway,
+		Subnet:  c.Conf.DockerInfo.Ipv4Subnet,
+		Gateway: c.Conf.DockerInfo.Ipv4Gateway,
 	}
 	ipamIPv6Config := network.IPAMConfig{
-		Subnet:  DockerInfo.Ipv6Subnet,
-		Gateway: DockerInfo.Ipv6Gateway,
+		Subnet:  c.Conf.DockerInfo.Ipv6Subnet,
+		Gateway: c.Conf.DockerInfo.Ipv6Gateway,
 	}
 	var ipamConfig []network.IPAMConfig
 	ipamConfig = append(ipamConfig, ipamIPv4Config)
@@ -62,11 +46,11 @@ func (d *Docker) createBridge() (err error) {
 
 	var bridgeName string
 	var netCreateResponse types.NetworkCreateResponse
-	netCreateResponse, err = d.cli.NetworkCreate(context.Background(), DockerInfo.Bridge, networkOptions)
+	netCreateResponse, err = c.DockerClient.NetworkCreate(context.Background(), c.Conf.DockerInfo.Bridge, networkOptions)
 	if err != nil {
 		if strings.Contains(err.Error(), "already exists") {
-			log.Debugf("Container network %s already exists", DockerInfo.Bridge)
-			netResource, err := d.cli.NetworkInspect(context.TODO(), DockerInfo.Bridge, types.NetworkInspectOptions{})
+			log.Debugf("Container network %s already exists", c.Conf.DockerInfo.Bridge)
+			netResource, err := c.DockerClient.NetworkInspect(context.TODO(), c.Conf.DockerInfo.Bridge, types.NetworkInspectOptions{})
 			if err != nil {
 				return err
 			}
@@ -85,7 +69,7 @@ func (d *Docker) createBridge() (err error) {
 		}
 		bridgeName = "br-" + netCreateResponse.ID[:12]
 	}
-	log.Debugf("container network %s : bridge name: %s", DockerInfo.Bridge, bridgeName)
+	log.Debugf("container network %s : bridge name: %s", c.Conf.DockerInfo.Bridge, bridgeName)
 	log.Debug("Disable RPF check on the docker host part1")
 	if err = sysctl.Set("net.ipv4.conf.default.rp_filter", "0"); err != nil {
 		return err
@@ -111,8 +95,8 @@ func (d *Docker) createBridge() (err error) {
 	return nil
 }
 
-func (d *Docker) deleteBridge() (err error) {
-	err = d.cli.NetworkRemove(context.Background(), DockerInfo.Bridge)
+func (c *cLab) deleteBridge() (err error) {
+	err = c.DockerClient.NetworkRemove(context.Background(), c.Conf.DockerInfo.Bridge)
 	if err != nil {
 		return err
 
@@ -120,11 +104,11 @@ func (d *Docker) deleteBridge() (err error) {
 	return nil
 }
 
-func (d *Docker) createContainer(name string, node *Node) (err error) {
+func (c *cLab) createContainer(name string, node *Node) (err error) {
 	log.Debug("Create container: ", name)
 	ctx := context.Background()
 
-	c, err := d.cli.ContainerCreate(ctx,
+	cont, err := c.DockerClient.ContainerCreate(ctx,
 		&container.Config{
 			Image:        node.Image,
 			Cmd:          strings.Fields(node.Cmd),
@@ -139,46 +123,50 @@ func (d *Docker) createContainer(name string, node *Node) (err error) {
 			Binds:       node.Binds,
 			Sysctls:     node.Sysctls,
 			Privileged:  true,
-			NetworkMode: container.NetworkMode(DockerInfo.Bridge),
-		}, nil, "lab"+"-"+Prefix+"-"+name)
+			NetworkMode: container.NetworkMode(c.Conf.DockerInfo.Bridge),
+		}, nil, "lab"+"-"+c.Conf.Prefix+"-"+name)
 	if err != nil {
 		return err
 	}
 	log.Debug(fmt.Sprintf("Container create response: %v", c))
 
-	node.Cid = c.ID
+	node.Cid = cont.ID
 
-	err = d.cli.ContainerStart(ctx,
-		c.ID,
-		types.ContainerStartOptions{
-			CheckpointID:  "",
-			CheckpointDir: "",
-		},
-	)
+	err = c.startContainer(name, node)
 	if err != nil {
 		return err
 	}
+	// err = c.DockerClient.ContainerStart(ctx,
+	// 	cont.ID,
+	// 	types.ContainerStartOptions{
+	// 		CheckpointID:  "",
+	// 		CheckpointDir: "",
+	// 	},
+	// )
+	// if err != nil {
+	// 	return err
+	// }
 
-	s, err := d.cli.ContainerInspect(ctx, c.ID)
-	if err != nil {
-		return err
-	}
-	node.Pid = s.State.Pid
-	node.MgmtIPv4 = s.NetworkSettings.Networks["srlinux_bridge"].IPAddress
-	node.MgmtIPv6 = s.NetworkSettings.Networks["srlinux_bridge"].GlobalIPv6Address
-	node.MgmtMac = s.NetworkSettings.Networks["srlinux_bridge"].MacAddress
+	// s, err := c.DockerClient.ContainerInspect(ctx, cont.ID)
+	// if err != nil {
+	// 	return err
+	// }
+	// node.Pid = s.State.Pid
+	// node.MgmtIPv4 = s.NetworkSettings.Networks["srlinux_bridge"].IPAddress
+	// node.MgmtIPv6 = s.NetworkSettings.Networks["srlinux_bridge"].GlobalIPv6Address
+	// node.MgmtMac = s.NetworkSettings.Networks["srlinux_bridge"].MacAddress
 
-	log.Debug("Container pid: ", node.Pid)
-	log.Debug("Container pid: ", node.MgmtIPv4)
+	// log.Debug("Container pid: ", node.Pid)
+	// log.Debug("Container pid: ", node.MgmtIPv4)
 
 	return nil
 }
 
-func (d *Docker) startContainer(name string, node *Node) (err error) {
+func (c *cLab) startContainer(name string, node *Node) (err error) {
 	log.Debug("Start container: ", name)
 	ctx := context.Background()
 
-	err = d.cli.ContainerStart(ctx,
+	err = c.DockerClient.ContainerStart(ctx,
 		node.Cid,
 		types.ContainerStartOptions{
 			CheckpointID:  "",
@@ -189,21 +177,27 @@ func (d *Docker) startContainer(name string, node *Node) (err error) {
 		return err
 	}
 
-	s, err := d.cli.ContainerInspect(ctx, node.Cid)
+	s, err := c.DockerClient.ContainerInspect(ctx, node.Cid)
 	if err != nil {
 		return err
 	}
 	node.Pid = s.State.Pid
-	log.Debug("Container pid: ", node.Pid)
+	node.MgmtIPv4 = s.NetworkSettings.Networks["srlinux_bridge"].IPAddress
+	node.MgmtIPv6 = s.NetworkSettings.Networks["srlinux_bridge"].GlobalIPv6Address
+	node.MgmtMac = s.NetworkSettings.Networks["srlinux_bridge"].MacAddress
+	log.Debugf("Container %s pid: %d ", name, node.Pid)
+	log.Debugf("Container %s mgmtIPv4: %s", name, node.MgmtIPv4)
+	log.Debugf("Container %s mgmtIPv6: %s", name, node.MgmtIPv6)
+	log.Debugf("Container %s mgmtMAC: %s", name, node.MgmtMac)
 
 	return nil
 }
 
-func (d *Docker) deleteContainer(name string, node *Node) (err error) {
+func (c *cLab) deleteContainer(name string, node *Node) (err error) {
 	log.Debug("Delete and remove container: ", name)
 	ctx := context.Background()
 
-	containers, err := d.cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	containers, err := c.DockerClient.ContainerList(ctx, types.ContainerListOptions{All: true})
 	if err != nil {
 		return err
 	}
@@ -211,7 +205,7 @@ func (d *Docker) deleteContainer(name string, node *Node) (err error) {
 
 	for _, container := range containers {
 		for _, n := range container.Names {
-			if strings.Contains(n, "lab"+"-"+Prefix+"-"+name) {
+			if strings.Contains(n, "lab"+"-"+c.Conf.Prefix+"-"+name) {
 				cid = container.ID
 				break
 			}
@@ -219,7 +213,7 @@ func (d *Docker) deleteContainer(name string, node *Node) (err error) {
 	}
 
 	if cid != "" {
-		err = d.cli.ContainerRemove(ctx, cid, types.ContainerRemoveOptions{Force: true})
+		err = c.DockerClient.ContainerRemove(ctx, cid, types.ContainerRemoveOptions{Force: true})
 		if err != nil {
 			return err
 		}
