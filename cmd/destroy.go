bc2745e73298131c01b218ad66ad9e47d8174630
karimra <karim.radhouani@nokia.com>
2020-09-06 16:12:17 +0800

    destroy topo by --prefix

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/srl-wim/container-lab/clab"
@@ -19,7 +22,31 @@ var destroyCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal(err)
 		}
-
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		if prefix != "" {
+			filter := filters.NewArgs()
+			filter.Add("label", fmt.Sprintf("containerlab=lab-%s", prefix))
+			containers, err := c.DockerClient.ContainerList(ctx, types.ContainerListOptions{
+				Filters: filter,
+			})
+			if err != nil {
+				log.Fatalf("could not list containers: %v", err)
+			}
+			var name string
+			for _, cont := range containers {
+				name = cont.ID
+				if len(cont.Names) > 0 {
+					name = cont.Names[0]
+				}
+				log.Infof("Removing container: %s", name)
+				err = c.DockerClient.ContainerRemove(ctx, cont.ID, types.ContainerRemoveOptions{})
+				if err != nil {
+					log.Errorf("could not remove container '%s': %v", name, err)
+				}
+			}
+			return
+		}
 		log.Info("Getting topology information ...")
 		if err = c.GetTopology(&topo); err != nil {
 			log.Fatal(err)
@@ -31,7 +58,6 @@ var destroyCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 
-		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
 		log.Info("Destroying container lab: ... ", topo)
@@ -58,4 +84,5 @@ var destroyCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(destroyCmd)
 	destroyCmd.Flags().StringVarP(&topo, "topo", "t", "/etc/containerlab/lab-examples/wan-topo.yml", "path to the file with topology information")
+	destroyCmd.Flags().StringVarP(&prefix, "prefix", "p", "", "lab name prefix")
 }
