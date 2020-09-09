91c0dc78b9b49db444fb662c4ee34f1be3d3f33c
steiler <github@vahlenkamp.net>
2020-09-09 11:30:16 +0000

    Using go-graphviz to generate not just the .dot but also a png file.
    Also fixing, that a topology would have to be deployed before a graph could be created, since the folder structure did not already exist.

--- clab/graph.go
+++ clab/graph.go
@@ -1,68 +1,93 @@
 package clab
 
 import (
+	"bytes"
 	"strings"
 
-	"github.com/awalterschulze/gographviz"
+	"github.com/goccy/go-graphviz"
+	"github.com/goccy/go-graphviz/cgraph"
 	log "github.com/sirupsen/logrus"
 )
 
-var g *gographviz.Graph
-
 // GenerateGraph generates a graph for the lab topology
 func (c *cLab) GenerateGraph(topo string) error {
-	log.Info("Generating lab graph ...")
-	g = gographviz.NewGraph()
-	if err := g.SetName(c.FileInfo.shortname); err != nil {
-		return err
+	h := graphviz.New()
+	graph, err := h.Graph()
+	if err != nil {
+		log.Fatal(err)
 	}
-	if err := g.SetDir(false); err != nil {
-		return err
-	}
+	defer func() {
+		if err := graph.Close(); err != nil {
+			log.Fatal(err)
+		}
+		h.Close()
+	}()
 
-	var attr map[string]string
-	attr = make(map[string]string)
-	attr["color"] = "red"
-	attr["style"] = "filled"
-	attr["fillcolor"] = "red"
+	graph.Attr(1, "style", "filled")  // kind 1 == node defaults
+	graph.Attr(1, "fillcolor", "red") // kind 1 == node defaults
 
+	var graphNodeMap = make(map[string]*cgraph.Node)
+
+	// Create Nodes
 	for nodeName, node := range c.Nodes {
-		attr["label"] = nodeName
-		attr["xlabel"] = node.Kind
-		attr["group"] = node.Group
+		n, err := graph.CreateNode(nodeName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		n.SetLabel(nodeName)
+		n.SetXLabel(node.Kind)
+		n.SetGroup(node.Group)
 
 		if strings.Contains(node.Kind, "srl") {
-			attr["fillcolor"] = "green"
+			n.SetFillColor("green")
 		}
 		if strings.Contains(node.Group, "bb") {
-			attr["fillcolor"] = "blue"
-		}
-		if err := g.AddNode(c.FileInfo.shortname, node.ShortName, attr); err != nil {
-			return err
+			n.SetFillColor("blue")
+			n.SetFontColor("white")
 		}
 
+		graphNodeMap[nodeName] = n
 	}
 
-	attr = make(map[string]string)
-	attr["color"] = "green"
-
+	// Create Edges between the Nodes
+	graph.Attr(2, "fillcolor", "green") // kind 2 == edge defaults
+	graph.Attr(2, "arrowhead", "none")  // kind 2 == edge defaults
+	graph.Attr(2, "arrowtail", "none")  // kind 2 == edge defaults
 	for _, link := range c.Links {
-		if strings.Contains(link.B.Node.ShortName, "client") {
-			attr["color"] = "blue"
+		aEnd, ok := graphNodeMap[link.A.Node.ShortName]
+		if !ok {
+			log.Fatal("Node ", link.A.EndpointName, " does not exist!")
 		}
-		if err := g.AddEdge(link.A.Node.ShortName, link.B.Node.ShortName, false, attr); err != nil {
-			return err
+		bEnd, ok := graphNodeMap[link.B.Node.ShortName]
+		if !ok {
+			log.Fatal("Node ", link.B.EndpointName, " does not exist!")
 		}
-
+		e, err := graph.CreateEdge("", aEnd, bEnd)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_ = e
+		// Print node and interface name in edge label
+		//e.SetLabel(link.A.Node.ShortName + " " + link.A.EndpointName + "\n" + link.B.Node.ShortName + " " + link.B.EndpointName)
 	}
 
 	// create graph directory
+	CreateDirectory(c.Dir.Lab, 0755)
 	CreateDirectory(c.Dir.LabGraph, 0755)
 
-	// create graph filename
-	file := c.Dir.LabGraph + "/" + c.FileInfo.name + ".dot"
+	// write .dot file
+	var buf bytes.Buffer
+	if err := h.Render(graph, "dot", &buf); err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(buf.String())
+	dotFile := c.Dir.LabGraph + "/" + c.FileInfo.name + ".dot"
+	createFile(dotFile, buf.String())
 
-	createFile(file, g.String())
-
+	pngFile := c.Dir.LabGraph + "/" + c.FileInfo.name + ".png"
+	if err := h.RenderFilename(graph, graphviz.PNG, pngFile); err != nil {
+		log.Fatal(err)
+	}
+	log.Info("Done")
 	return nil
 }
