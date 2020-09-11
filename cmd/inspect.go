ad1612533babc8287ace2e33bcbed29ccf251b43
karimra <karim.radhouani@nokia.com>
2020-09-11 10:24:47 +0800

    add table output

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -4,11 +4,26 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/olekukonko/tablewriter"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/srl-wim/container-lab/clab"
 )
+
+var format string
+
+type containerDetails struct {
+	Name        string
+	Image       string
+	Kind        string
+	Group       string
+	State       string
+	IPv4Address string
+	IPv6Address string
+}
+type BridgeDetails struct{}
 
 // inspectCmd represents the inspect command
 var inspectCmd = &cobra.Command{
@@ -42,11 +57,60 @@ var inspectCmd = &cobra.Command{
 			log.Println("no containers found")
 			return
 		}
-		b, err := json.MarshalIndent(containers, "", "  ")
-		if err != nil {
-			log.Fatalf("failed to marshal containers struct: %v", err)
+		if format == "json" {
+			b, err := json.MarshalIndent(containers, "", "  ")
+			if err != nil {
+				log.Fatalf("failed to marshal containers struct: %v", err)
+			}
+			fmt.Println(string(b))
+			return
 		}
-		fmt.Println(string(b))
+		details := make([]containerDetails, 0, len(containers))
+		for _, cont := range containers {
+			cdet := containerDetails{
+				Image: cont.Image,
+				State: cont.State,
+			}
+			if len(cont.Names) > 0 {
+				cdet.Name = cont.Names[0]
+			}
+			if kind, ok := cont.Labels["kind"]; ok {
+				cdet.Kind = kind
+			}
+			if group, ok := cont.Labels["group"]; ok {
+				cdet.Group = group
+			}
+			if cont.NetworkSettings != nil {
+				if c.Conf.DockerInfo.Bridge != "" {
+					if br, ok := cont.NetworkSettings.Networks[c.Conf.DockerInfo.Bridge]; ok {
+						cdet.IPv4Address = fmt.Sprintf("%s/%d", br.IPAddress, br.IPPrefixLen)
+						cdet.IPv6Address = fmt.Sprintf("%s/%d", br.GlobalIPv6Address, br.GlobalIPv6PrefixLen)
+					}
+				}
+				if cdet.IPv4Address == "" && cdet.IPv6Address == "" {
+					for _, br := range cont.NetworkSettings.Networks {
+						cdet.IPv4Address = fmt.Sprintf("%s/%d", br.IPAddress, br.IPPrefixLen)
+						cdet.IPv6Address = fmt.Sprintf("%s/%d", br.GlobalIPv6Address, br.GlobalIPv6PrefixLen)
+						break
+					}
+				}
+			}
+			details = append(details, cdet)
+		}
+		tabData := toTableData(details)
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{
+			"Name",
+			"Image",
+			"Kind",
+			"Group",
+			"State",
+			"IPv4Address",
+			"IPv6Address",
+		})
+		table.SetAutoWrapText(false)
+		table.AppendBulk(tabData)
+		table.Render()
 	},
 }
 
@@ -55,4 +119,13 @@ func init() {
 
 	inspectCmd.Flags().StringVarP(&topo, "topo", "t", "/etc/containerlab/lab-examples/wan-topo.yml", "path to the file with topology information")
 	inspectCmd.Flags().StringVarP(&prefix, "prefix", "p", "", "lab name prefix")
+	inspectCmd.Flags().StringVarP(&format, "format", "f", "", "lab name prefix")
+}
+
+func toTableData(det []containerDetails) [][]string {
+	tabData := make([][]string, 0, len(det))
+	for _, d := range det {
+		tabData = append(tabData, []string{d.Name, d.Image, d.Kind, d.Group, d.State, d.IPv4Address, d.IPv6Address})
+	}
+	return tabData
 }
