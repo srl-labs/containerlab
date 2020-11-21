4e0bed20fbb642a8a183b822d5b289943efea3bb
hellt <dodin.roman@gmail.com>
2020-11-21 23:02:09 +0200

    added config file mount for ceos

--- clab/config.go
+++ clab/config.go
@@ -340,6 +340,10 @@ func (c *cLab) NewNode(nodeName string, nodeCfg NodeConfig, idx int) error {
 		node.Sysctls["net.ipv6.conf.all.autoconf"] = "0"
 		node.Sysctls["net.ipv6.conf.default.autoconf"] = "0"
 
+		// mount config file
+		cfgPath := filepath.Join(node.LabDir, "config", "startup-config")
+		node.Binds = append(node.Binds, fmt.Sprint(cfgPath, ":/mnt/flash/startup-config"))
+
 	case "srl":
 		// initialize the global parameters with defaults, can be overwritten later
 		node.Config = c.configInitialization(&nodeCfg, node.Kind)
