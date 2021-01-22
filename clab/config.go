bc6f3ae1ecf9accb70709075b7191a23b2e775ff
hellt <dodin.roman@gmail.com>
2021-01-22 17:07:09 +0200

    added xrv kind

--- clab/config.go
+++ clab/config.go
@@ -27,7 +27,7 @@ const (
 )
 
 // supported kinds
-var kinds = []string{"srl", "ceos", "linux", "alpine", "bridge"}
+var kinds = []string{"srl", "ceos", "crpd", "vr-sros", "vr-vmx", "vr-xrv", "linux", "bridge"}
 
 var defaultConfigTemplates = map[string]string{
 	"srl":  "/etc/containerlab/templates/srl/srlconfig.tpl",
@@ -545,6 +545,22 @@ func (c *CLab) NewNode(nodeName string, nodeCfg NodeConfig, idx int) error {
 		defEnv := map[string]string{
 			"USERNAME":        "admin",
 			"PASSWORD":        "admin@123",
+			"CONNECTION_MODE": "bridge",
+		}
+		node.Env = mergeStringMaps(defEnv, envs)
+
+		node.Cmd = fmt.Sprintf("--username %s --password %s --hostname %s --connection-mode %s --trace", node.Env["USERNAME"], node.Env["PASSWORD"], node.ShortName, node.Env["CONNECTION_MODE"])
+
+	case "vr-xrv":
+		node.Image = c.imageInitialization(&nodeCfg, node.Kind)
+		node.Group = c.groupInitialization(&nodeCfg, node.Kind)
+		node.Position = c.positionInitialization(&nodeCfg, node.Kind)
+		node.User = user
+
+		// env vars are used to set launch.py arguments in vrnetlab container
+		defEnv := map[string]string{
+			"USERNAME":        "clab",
+			"PASSWORD":        "clab@123",
 			"CONNECTION_MODE": "bridge",
 		}
 		node.Env = mergeStringMaps(defEnv, envs)
