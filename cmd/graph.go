b7e46247732afda7a93cba67761195b0d3d1827e
hellt <dodin.roman@gmail.com>
2021-01-23 18:48:03 +0200

    make graph possible in offline mode

--- cmd/graph.go
+++ cmd/graph.go
@@ -9,6 +9,7 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/docker/docker/api/types"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/srl-wim/container-lab/clab"
@@ -20,6 +21,8 @@ const (
 
 var srv string
 var tmpl string
+var offline bool
+var dot bool
 
 type graphTopo struct {
 	Nodes []containerDetails `json:"nodes,omitempty"`
@@ -41,6 +44,7 @@ type topoData struct {
 var graphCmd = &cobra.Command{
 	Use:   "graph",
 	Short: "generate a topology graph",
+	Long:  "generate topology graph based on the topology definition file and running containers\nreference: https://containerlab.srlinux.dev/cmd/graph/",
 
 	RunE: func(cmd *cobra.Command, args []string) error {
 		opts := []clab.ClabOption{
@@ -56,7 +60,7 @@ var graphCmd = &cobra.Command{
 			return err
 		}
 
-		if srv == "" {
+		if dot {
 			if err := c.GenerateGraph(topo); err != nil {
 				return err
 			}
@@ -66,34 +70,28 @@ var graphCmd = &cobra.Command{
 			Nodes: make([]containerDetails, 0, len(c.Nodes)),
 			Links: make([]link, 0, len(c.Links)),
 		}
+
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
-		containers, err := c.ListContainers(ctx, []string{fmt.Sprintf("containerlab=lab-%s", c.Config.Name)})
-		if err != nil {
-			log.Errorf("could not list containers: %v", err)
-		}
-		log.Debugf("found %d containers", len(containers))
-		if len(containers) == 0 {
-			return fmt.Errorf("no containers found, is the lab deployed?")
-		}
-		for _, cont := range containers {
-			var name string
-			if len(cont.Names) > 0 {
-				name = strings.TrimPrefix(cont.Names[0], fmt.Sprintf("/clab-%s-", c.Config.Name))
+
+		var containers []types.Container
+		// if offline mode is not enforced, list containers matching lab name
+		if !offline {
+			var err error
+			containers, err = c.ListContainers(ctx, []string{fmt.Sprintf("containerlab=lab-%s", c.Config.Name)})
+			if err != nil {
+				log.Errorf("could not list containers: %v", err)
 			}
-			log.Debugf("looking for node name %s", name)
-			if node, ok := c.Nodes[name]; ok {
-				gtopo.Nodes = append(gtopo.Nodes, containerDetails{
-					Name:        name,
-					Kind:        node.Kind,
-					Image:       cont.Image,
-					Group:       node.Group,
-					State:       fmt.Sprintf("%s/%s", cont.State, cont.Status),
-					IPv4Address: getContainerIPv4(cont, c.Config.Mgmt.Network),
-					IPv6Address: getContainerIPv6(cont, c.Config.Mgmt.Network),
-				})
-			}
+			log.Debugf("found %d containers", len(containers))
 		}
+
+		switch {
+		case len(containers) == 0:
+			buildGraphFromTopo(&gtopo, c)
+		case len(containers) > 0:
+			buildGraphFromDeployedLab(&gtopo, c, containers)
+		}
+
 		sort.Slice(gtopo.Nodes, func(i, j int) bool {
 			return gtopo.Nodes[i].Name < gtopo.Nodes[j].Name
 		})
@@ -128,8 +126,47 @@ var graphCmd = &cobra.Command{
 	},
 }
 
+func buildGraphFromTopo(g *graphTopo, c *clab.CLab) {
+	log.Info("building graph from topology file")
+	for _, node := range c.Nodes {
+		g.Nodes = append(g.Nodes, containerDetails{
+			Name:        node.ShortName,
+			Kind:        node.Kind,
+			Image:       node.Image,
+			Group:       node.Group,
+			State:       "N/A",
+			IPv4Address: node.MgmtIPv4Address,
+			IPv6Address: node.MgmtIPv6Address,
+		})
+	}
+
+}
+
+func buildGraphFromDeployedLab(g *graphTopo, c *clab.CLab, containers []types.Container) {
+	for _, cont := range containers {
+		var name string
+		if len(cont.Names) > 0 {
+			name = strings.TrimPrefix(cont.Names[0], fmt.Sprintf("/clab-%s-", c.Config.Name))
+		}
+		log.Debugf("looking for node name %s", name)
+		if node, ok := c.Nodes[name]; ok {
+			g.Nodes = append(g.Nodes, containerDetails{
+				Name:        name,
+				Kind:        node.Kind,
+				Image:       cont.Image,
+				Group:       node.Group,
+				State:       fmt.Sprintf("%s/%s", cont.State, cont.Status),
+				IPv4Address: getContainerIPv4(cont, c.Config.Mgmt.Network),
+				IPv6Address: getContainerIPv6(cont, c.Config.Mgmt.Network),
+			})
+		}
+	}
+}
+
 func init() {
 	rootCmd.AddCommand(graphCmd)
-	graphCmd.Flags().StringVarP(&srv, "srv", "s", "", "HTTP server address to view, customize and export your topology")
+	graphCmd.Flags().StringVarP(&srv, "srv", "s", ":50080", "HTTP server address to view, customize and export your topology")
+	graphCmd.Flags().BoolVarP(&offline, "offline", "o", false, "use only information from topo file when building graph")
+	graphCmd.Flags().BoolVarP(&dot, "dot", "", false, "generate dot file instead of launching the web server")
 	graphCmd.Flags().StringVarP(&tmpl, "template", "", defaultTemplatePath, "Go html template used to generate the graph")
 }
