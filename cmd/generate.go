3f93fbb1c4c3343c96fc14ab85c7805a365dc149
Roman Dodin <dodin.roman@gmail.com>
2021-02-19 10:40:10 +0000

    aligned kinds

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -30,12 +15,17 @@ import (
 )
 
 var interfaceFormat = map[string]string{
-	"srl":    "e1-%d",
-	"ceos":   "eth%d",
-	"linux":  "eth$d",
-	"bridge": "veth%d",
+	"srl":      "e1-%d",
+	"ceos":     "eth%d",
+	"crpd":     "eth%d",
+	"sonic-vs": "eth%d",
+	"linux":    "eth%d",
+	"bridge":   "veth%d",
+	"vr-sros":  "eth%d",
+	"vr-vmx":   "eth%d",
+	"vr-xrv9k": "eth%d",
 }
-var supportedKinds = []string{"srl", "ceos", "linux", "bridge", "sonic", "crpd"}
+var supportedKinds = []string{"srl", "ceos", "linux", "bridge", "sonic-vs", "crpd", "vr-sros", "vr-vmx", "vr-xrv9k"}
 
 const (
 	defaultSRLType     = "ixr6"
