7aa102bf34ddc3917a54e176425616384541bbe2
Roman Dodin <dodin.roman@gmail.com>
2021-03-05 08:59:28 +0000

    scoped out sonic node init

--- clab/config.go
+++ clab/config.go
@@ -462,18 +462,10 @@ func (c *CLab) NewNode(nodeName string, nodeCfg NodeConfig, idx int) error {
 			return err
 		}
 	case "sonic-vs":
-		node.Config, err = c.configInit(&nodeCfg, node.Kind)
+		err = initSonicNode(c, nodeCfg, node, user, envs)
 		if err != nil {
 			return err
 		}
-		node.Image = c.imageInitialization(&nodeCfg, node.Kind)
-		node.Group = c.groupInitialization(&nodeCfg, node.Kind)
-		node.Position = c.positionInitialization(&nodeCfg, node.Kind)
-		node.User = user
-
-		// rewrite entrypoint so sonic won't start supervisord before we attach veth interfaces
-		node.Entrypoint = "/bin/bash"
-
 	case "vr-sros":
 		err = initSROSNode(c, nodeCfg, node, user, envs)
 		if err != nil {
---
+++ clab/sonic.go
@@ -0,0 +1,19 @@
+package clab
+
+func initSonicNode(c *CLab, nodeCfg NodeConfig, node *Node, user string, envs map[string]string) error {
+	var err error
+
+	node.Config, err = c.configInit(&nodeCfg, node.Kind)
+	if err != nil {
+		return err
+	}
+	node.Image = c.imageInitialization(&nodeCfg, node.Kind)
+	node.Group = c.groupInitialization(&nodeCfg, node.Kind)
+	node.Position = c.positionInitialization(&nodeCfg, node.Kind)
+	node.User = user
+
+	// rewrite entrypoint so sonic won't start supervisord before we attach veth interfaces
+	node.Entrypoint = "/bin/bash"
+
+	return err
+}
