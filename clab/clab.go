aef4c72192dbdb55d4bfbb6b548a5486a0a45908
Roman Dodin <dodin.roman@gmail.com>
2021-05-11 14:40:00 +0200

    hard stop when topo file is not present

--- clab/clab.go
+++ clab/clab.go
@@ -61,7 +61,7 @@ func WithRuntime(name string, d bool, dur time.Duration, gracefulShutdown bool) 
 func WithTopoFile(file string) ClabOption {
 	return func(c *CLab) {
 		if file == "" {
-			return
+			log.Fatal("provide a path to the clab toplogy file")
 		}
 		if err := c.GetTopology(file); err != nil {
 			log.Fatalf("failed to read topology file: %v", err)
@@ -69,7 +69,7 @@ func WithTopoFile(file string) ClabOption {
 
 		err := c.initMgmtNetwork()
 		if err != nil {
-			log.Fatalf("initMgmtNetwork: %s", err)
+			log.Fatalf("failed to init the management network: %s", err)
 		}
 
 	}
