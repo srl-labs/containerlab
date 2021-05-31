96cca29cab3b143a458c9547e4fb09867978e982
Roman Dodin <dodin.roman@gmail.com>
2021-05-31 08:37:05 +0200

    added check for interactive mode

--- get.sh
+++ get.sh
@@ -125,7 +125,9 @@ checkInstalledVersion() {
             echo "A newer ${BINARY_NAME} ${TAG_WO_VER} is available. Release notes: https://containerlab.srlinux.dev/rn/${TAG_WO_VER}"
             echo "You are running containerlab $version version"
             UPGR_NEEDED="Y"
-            read -e -p "Proceed with upgrade? [Y/n]: " -i "Y" UPGR_NEEDED
+            if [ -t 0 ]; then
+                read -e -p "Proceed with upgrade? [Y/n]: " -i "Y" UPGR_NEEDED
+            fi
             if [ "$UPGR_NEEDED" == "Y" ]; then
                 return 1
             fi
