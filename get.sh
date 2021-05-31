3730032c90e673efaec5d762a361dc17ef414b1b
Roman Dodin <dodin.roman@gmail.com>
2021-05-31 08:34:13 +0200

    added default value

--- get.sh
+++ get.sh
@@ -124,6 +124,7 @@ checkInstalledVersion() {
         else
             echo "A newer ${BINARY_NAME} ${TAG_WO_VER} is available. Release notes: https://containerlab.srlinux.dev/rn/${TAG_WO_VER}"
             echo "You are running containerlab $version version"
+            UPGR_NEEDED="Y"
             read -e -p "Proceed with upgrade? [Y/n]: " -i "Y" UPGR_NEEDED
             if [ "$UPGR_NEEDED" == "Y" ]; then
                 return 1
