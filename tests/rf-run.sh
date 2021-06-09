6defe7d4e1cec7bff2c815338e3d063a67f2acfa
Roman Dodin <dodin.roman@gmail.com>
2021-06-09 20:52:07 +0200

    added license

--- tests/rf-run.sh
+++ tests/rf-run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+# Copyright 2020 Nokia
+# Licensed under the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+
 
 robot --consolecolors on -r none -l ./tests/out/$(basename $1)-log --output ./tests/out/$(basename $1)-out $1
