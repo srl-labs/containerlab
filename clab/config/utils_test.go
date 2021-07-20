4bdc655f3243f2ea2e9924fc7c436c49f2c88437
Johann Kellerman kellerza@gmail.com <kellerza@gmail.com>
2021-07-20 17:13:08 +0200

    Some tests

--- clab/config/utils_test.go
+++ clab/config/utils_test.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"runtime"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/srl-labs/containerlab/types"
 	"inet.af/netaddr"
 )
 
@@ -27,6 +30,12 @@ func TestFarEndIP(t *testing.T) {
 
 	for k, v := range lst {
 		n := ipFarEnd(netaddr.MustParseIPPrefix(k))
+		n2, _ := ipFarEndS(k)
+
+		if n2 != n.String() {
+			t.Errorf("far end %s != %s, expected %s", n, n2, v)
+		}
+
 		if n.IsZero() && v == "" {
 			continue
 		}
@@ -51,4 +60,78 @@ func TestIPLastOctect(t *testing.T) {
 		}
 	}
 
+}
+
+func gettestLink() *types.Link {
+	return &types.Link{
+		A: &types.Endpoint{
+			Node: &types.NodeConfig{
+				ShortName: "a",
+				Config: &types.ConfigDispatcher{
+					Vars: map[string]interface{}{
+						"systemip": "10.0.0.1/32",
+					},
+				},
+			},
+		},
+		B: &types.Endpoint{
+			Node: &types.NodeConfig{
+				ShortName: "b",
+				Config: &types.ConfigDispatcher{
+					Vars: map[string]interface{}{
+						"systemip": "10.0.0.2/32",
+					},
+				},
+			},
+		},
+		Vars: map[string]interface{}{},
+	}
+}
+
+func assert(t *testing.T, val, exp interface{}) {
+	if !cmp.Equal(val, exp) {
+		_, fn, line, _ := runtime.Caller(1)
+		t.Errorf("assert failed on line %v in %s\n%s", line, fn, cmp.Diff(val, exp))
+	}
+}
+
+func TestLinkName(t *testing.T) {
+	l := gettestLink()
+	n1, n2, _ := linkName(l)
+	assert(t, n1, "to_b")
+	assert(t, n2, "to_a")
+
+	l.Vars["linknr"] = 1
+	n1, n2, _ = linkName(l)
+	assert(t, n1, "to_b_1")
+	assert(t, n2, "to_a_1")
+}
+
+func TestLinkIP(t *testing.T) {
+	l := gettestLink()
+	n1, n2, _ := linkIP(l)
+	assert(t, n1, "1.1.2.0/31")
+	assert(t, n2, "1.1.2.1/31")
+
+	l.Vars["linknr"] = 1
+	n1, n2, _ = linkIP(l)
+	assert(t, n1, "1.1.2.2/31")
+	assert(t, n2, "1.1.2.3/31")
+}
+
+func TestPrepareLinkVars(t *testing.T) {
+	a := make(Dict)
+	b := make(Dict)
+	l := gettestLink()
+	_ = prepareLinkVars(0, l, a, b)
+	assert(t, a, Dict{
+		"far":  Dict{"ip": "1.1.2.1/31", "name": "to_a", "node": "b"},
+		"ip":   "1.1.2.0/31",
+		"name": "to_b",
+	})
+	assert(t, b, Dict{
+		"far":  Dict{"ip": "1.1.2.0/31", "name": "to_b", "node": "a"},
+		"ip":   "1.1.2.1/31",
+		"name": "to_a",
+	})
 }
