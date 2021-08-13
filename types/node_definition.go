485ecb1e747a1a083da345f688772bd2313e7731
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-08-13 10:42:48 -0500

    Remove default

--- types/node_definition.go
+++ types/node_definition.go
@@ -6,7 +6,7 @@ type NodeDefinition struct {
 	Group         string            `yaml:"group,omitempty"`
 	Type          string            `yaml:"type,omitempty"`
 	StartupConfig string            `yaml:"startup-config,omitempty"`
-	StartupDelay  uint              `default:0 yaml:"startup-delay"`
+	StartupDelay  uint              `yaml:"startup-delay"`
 	Config        *ConfigDispatcher `yaml:"config,omitempty"`
 	Image         string            `yaml:"image,omitempty"`
 	License       string            `yaml:"license,omitempty"`
