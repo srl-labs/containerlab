fa58d6b321dd3a60c4840f5b14a8616eec5e2589
Karim Radhouani <medkarimrdi@gmail.com>
2021-09-07 07:52:10 -0700

    avoid panic when importing Envs if NodeDefinition is nil

--- types/node_definition.go
+++ types/node_definition.go
@@ -238,11 +238,16 @@ func (n *NodeDefinition) GetNodeRAM() string {
 // ImportEnvs imports all environment variales defined in the shell
 // if __IMPORT_ENVS is set to true
 func (n *NodeDefinition) ImportEnvs() {
+	if n == nil || n.Env == nil {
+		return
+	}
+
 	var importEnvs bool
 
 	for k, v := range n.Env {
 		if k == importEnvsKey && v == "true" {
 			importEnvs = true
+			break
 		}
 	}
 
