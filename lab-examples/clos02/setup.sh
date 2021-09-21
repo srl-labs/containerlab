3d05f902e4bb9351f66b0c0b3494d3e3816fe95b
Siva Sivakumar <siva.sivakumar@nokia.com>
2021-09-22 00:22:42 +0530

    adding parallelism to config push

--- lab-examples/clos02/setup.sh
+++ lab-examples/clos02/setup.sh
@@ -2,63 +2,47 @@
 
 CFG_DIR=./configs
 
-configure_SRL () {
+configure_SRL() {
   OUT=$(gnmic -a clab-clos02-$1 --timeout 30s -u admin -p admin -e json_ietf --skip-verify set --update-path / --update-file $CFG_DIR/$1.yaml 2>&1)
   echo $OUT | grep -q -e '\"operation\": \"UPDATE\"'
   if [ $? -eq 0 ]; then
-    SAVE=$(docker exec -it clab-clos02-$1 sr_cli "save startup" 2>&1)
+    docker exec clab-clos02-$1 sr_cli "save startup" > /dev/null
   else
-    echo "Error: Unable to push config. Check if NE is reachable with gRPC enabled."
+    echo "Error: Unable to push config into clab-clos02-$1."
   fi
+  echo $OUT > /dev/null
 }
 
-if [ $# -eq 0 ]; then
-  echo "Error: No topology file passed."
-else
-  DEPLOY=$(containerlab deploy --topo $1 2>&1)
-  echo $DEPLOY | grep -q -e 'Adding containerlab host entries'
-  if [ $? -eq 0 ]; then
-    echo
-    echo "Containerlab (clos02) created:"
-    containerlab inspect --topo $1
-    echo
-    echo "01 Configuring leaf1..."
-    configure_SRL "leaf1"
-    echo "02 Configuring leaf2..."
-    configure_SRL "leaf2"
-    echo "03 Configuring leaf3..."
-    configure_SRL "leaf3"
-    echo "04 Configuring leaf4..."
-    configure_SRL "leaf4"
-    echo "05 Configuring spine1..."
-    configure_SRL "spine1"
-    echo "06 Configuring spine2..."
-    configure_SRL "spine2"
-    echo "07 Configuring spine3..."
-    configure_SRL "spine3"
-    echo "08 Configuring spine4..."
-    configure_SRL "spine4"
-    echo "09 Configuring superspine1..."
-    configure_SRL "superspine1"
-    echo "10 Configuring superspine2..."
-    configure_SRL "superspine2"
-    echo
-    echo "11 Configuring client1 IP addressing..."
-    docker cp $CFG_DIR/client1.sh clab-clos02-client1:/tmp/
-    docker exec -it clab-clos02-client1 bash /tmp/client1.sh
-    echo "12 Configuring client2 IP addressing..."
-    docker cp $CFG_DIR/client2.sh clab-clos02-client2:/tmp/
-    docker exec -it clab-clos02-client2 bash /tmp/client2.sh
-    echo "13 Configuring client3 IP addressing..."
-    docker cp $CFG_DIR/client3.sh clab-clos02-client3:/tmp/
-    docker exec -it clab-clos02-client3 bash /tmp/client3.sh
-    echo "14 Configuring client4 IP addressing..."
-    docker cp $CFG_DIR/client4.sh clab-clos02-client4:/tmp/
-    docker exec -it clab-clos02-client4 bash /tmp/client4.sh
-    echo
-    echo "Success: Setup finished"
-    echo
+configure_CLIENT() {
+  docker cp $CFG_DIR/$1.sh clab-clos02-$1:/tmp/
+  docker exec clab-clos02-$1 bash /tmp/$1.sh 2>/dev/null
+}
+
+echo
+PIDS=""
+NE=("leaf1" "leaf2" "leaf3" "leaf4" "spine1" "spine2" "spine3" "spine4" "superspine1" "superspine2")
+CLIENT=("client1" "client2" "client3" "client4")
+
+for VARIANT in ${NE[@]}; do
+  ( configure_SRL $VARIANT ) &
+  REF=$!
+  echo "[$REF] Configuring $VARIANT..."
+  PIDS+=" $REF"
+done
+
+for VARIANT in ${CLIENT[@]}; do
+  ( configure_CLIENT $VARIANT ) &
+  REF=$!
+  echo "[$REF] Configuring $VARIANT..."
+  PIDS+=" $REF"
+done
+
+echo
+for p in $PIDS; do
+  if wait $p; then
+    echo "Process $p success"
   else
-    echo "Error: Not a valid topology file."
+    echo "Process $p fail"
   fi
-fi
+done
+echo
