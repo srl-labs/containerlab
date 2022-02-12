4113fef2c7d4b2d762bb6e1ed4f87cd38404b8db
Rudranil <rudranil.das@keysight.com>
2022-02-12 09:30:17 +0000

    Pause ixia-c deployment till deployed container says it is ready

--- nodes/ixiac_one/ixiac-one.go
+++ nodes/ixiac_one/ixiac-one.go
@@ -6,11 +6,24 @@ package ixiac_one
 
 import (
 	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/srl-labs/containerlab/nodes"
 	"github.com/srl-labs/containerlab/runtime"
 	"github.com/srl-labs/containerlab/types"
 )
+
+var ixiacStatusConfig = struct {
+	statusSleepDuration time.Duration
+	statusInProgressMsg string
+}{
+	statusSleepDuration: time.Duration(time.Second * 5),
+	statusInProgressMsg: "ls: ./.ready: No such file or directory",
+}
 
 func init() {
 	nodes.Register(nodes.NodeKindIXIACONE, func() nodes.Node {
@@ -41,7 +54,10 @@ func (l *ixiacOne) Deploy(ctx context.Context) error {
 	return err
 }
 
-func (l *ixiacOne) PostDeploy(_ context.Context, _ map[string]nodes.Node) error { return nil }
+func (l *ixiacOne) PostDeploy(ctx context.Context, _ map[string]nodes.Node) error {
+	log.Infof("Running postdeploy actions for ixia-c '%s' node", l.cfg.ShortName)
+	return ixiacPostDeploy(ctx, l.runtime, l.cfg)
+}
 
 func (l *ixiacOne) GetImages() map[string]string {
 	images := make(map[string]string)
@@ -58,5 +74,28 @@ func (l *ixiacOne) Delete(ctx context.Context) error {
 }
 
 func (*ixiacOne) SaveConfig(_ context.Context) error {
+	return nil
+}
+
+// ixiacPostDeploy runs postdeploy actions which are required for ixia-c node
+func ixiacPostDeploy(_ context.Context, r runtime.ContainerRuntime, node *types.NodeConfig) error {
+	// TODO: replace following by goroutine
+	for {
+		readyCmd := "ls ./.ready"
+		bashcmd := fmt.Sprintf("docker exec %s %s", node.LongName, readyCmd)
+		cmd := exec.Command("/bin/sh", "-c", bashcmd)
+		//fmt.Println("---Cmd: ", cmd)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			msg := strings.TrimSuffix(string(out), "\n")
+			if msg != ixiacStatusConfig.statusInProgressMsg {
+				return err
+			}
+			time.Sleep(ixiacStatusConfig.statusSleepDuration)
+		} else {
+			break
+		}
+	}
+
 	return nil
 }
