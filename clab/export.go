bb04c75723de9b6521c65fb7376470c30f98c3fa
Alex Bortok <dev@bortok.com>
2022-03-27 14:01:27 -0700

    use json marshal to export topoGraph

--- clab/export.go
+++ clab/export.go
@@ -5,14 +5,25 @@
 package clab
 
 import (
+	"encoding/json"
 	"io"
 	"os"
 	"path/filepath"
-	"sort"
-	"text/template"
 
 	"github.com/srl-labs/containerlab/types"
 )
+
+type topoGraph struct {
+	Nodes map[string]*types.NodeConfig `json:"nodes,omitempty"`
+	//	Links []link `json:"links,omitempty"`
+}
+
+type link struct {
+	Source         string `json:"source,omitempty"`
+	SourceEndpoint string `json:"source_endpoint,omitempty"`
+	Target         string `json:"target,omitempty"`
+	TargetEndpoint string `json:"target_endpoint,omitempty"`
+}
 
 // GenerateExports generate various export files and writes it to a lab location
 func (c *CLab) GenerateExports() error {
@@ -26,57 +37,24 @@ func (c *CLab) GenerateExports() error {
 
 // generateTopologyGraph generates and writes topology graph file to w
 func (c *CLab) generateTopologyGraph(w io.Writer) error {
-
-	invT :=
-		`{
-      "nodes": [
-        {{- range $nodes}}
-      ],
-      "links": [
-      ]
-}
-`
-	type inv struct {
-		// clab nodes aggregated by their kind
-		Nodes map[string][]*types.NodeConfig
-		// clab nodes aggregated by user-defined groups
-		Groups map[string][]*types.NodeConfig
-	}
-
-	i := inv{
-		Nodes:  make(map[string][]*types.NodeConfig),
-		Groups: make(map[string][]*types.NodeConfig),
+	g := topoGraph{
+		Nodes: make(map[string]*types.NodeConfig),
+		//Links: make([]c.Link, 0, len(c.Links)),
 	}
 
 	for _, n := range c.Nodes {
-		i.Nodes[n.Config().Kind] = append(i.Nodes[n.Config().Kind], n.Config())
-		if n.Config().Labels["ansible-group"] != "" {
-			i.Groups[n.Config().Labels["ansible-group"]] = append(i.Groups[n.Config().Labels["ansible-group"]], n.Config())
-		}
+		cfg := n.Config()
+		// Empty NodeConfig.Endpoints slice to avoid cyclic references incompatible with json.Marshal()
+		cfg.Endpoints = make([]types.Endpoint, 0, 0)
+		g.Nodes[n.Config().ShortName] = cfg
 	}
 
-	// sort nodes by name as they are not sorted originally
-	for _, nodes := range i.Nodes {
-		sort.Slice(nodes, func(i, j int) bool {
-			return nodes[i].ShortName < nodes[j].ShortName
-		})
-	}
-
-	// sort nodes-per-group by name as they are not sorted originally
-	for _, nodes := range i.Groups {
-		sort.Slice(nodes, func(i, j int) bool {
-			return nodes[i].ShortName < nodes[j].ShortName
-		})
-	}
-
-	t, err := template.New("graph").Parse(invT)
+	b, err := json.Marshal(g)
 	if err != nil {
 		return err
 	}
-	err = t.Execute(w, i)
-	if err != nil {
-		return err
-	}
-	return err
 
+	w.Write(b)
+
+	return nil
 }
