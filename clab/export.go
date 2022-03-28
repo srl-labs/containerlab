3305c8867c2f910b498008cf90c90c04eb3291c8
Alex Bortok <dev@bortok.com>
2022-03-27 18:57:16 -0700

    added links to generateTopologyGraph

--- clab/export.go
+++ clab/export.go
@@ -15,14 +15,7 @@ import (
 
 type topoGraph struct {
 	Nodes map[string]*types.NodeConfig `json:"nodes,omitempty"`
-	//	Links []link `json:"links,omitempty"`
-}
-
-type link struct {
-	Source         string `json:"source,omitempty"`
-	SourceEndpoint string `json:"source_endpoint,omitempty"`
-	Target         string `json:"target,omitempty"`
-	TargetEndpoint string `json:"target_endpoint,omitempty"`
+	Links []Link                       `json:"links,omitempty"`
 }
 
 // GenerateExports generate various export files and writes it to a lab location
@@ -39,7 +32,7 @@ func (c *CLab) GenerateExports() error {
 func (c *CLab) generateTopologyGraph(w io.Writer) error {
 	g := topoGraph{
 		Nodes: make(map[string]*types.NodeConfig),
-		//Links: make([]c.Link, 0, len(c.Links)),
+		Links: make([]Link, 0, len(c.Links)),
 	}
 
 	for _, n := range c.Nodes {
@@ -47,6 +40,15 @@ func (c *CLab) generateTopologyGraph(w io.Writer) error {
 		// Empty NodeConfig.Endpoints slice to avoid cyclic references incompatible with json.Marshal()
 		cfg.Endpoints = make([]types.Endpoint, 0, 0)
 		g.Nodes[n.Config().ShortName] = cfg
+	}
+
+	for _, l := range c.Links {
+		g.Links = append(g.Links, Link{
+			Source:         l.A.Node.ShortName,
+			SourceEndpoint: l.A.EndpointName,
+			Target:         l.B.Node.ShortName,
+			TargetEndpoint: l.B.EndpointName,
+		})
 	}
 
 	b, err := json.Marshal(g)
