bb6f64ee0fb3e645cb009d2d1bb4c39f204c37c2
Roman Dodin <dodin.roman@gmail.com>
2022-04-07 20:25:06 +0200

    rever pointer usage

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -242,13 +242,13 @@ func deployFn(_ *cobra.Command, _ []string) error {
 	// exec commands specified for containers with `exec` parameter
 	execJSONResult := make(map[string]map[string]map[string]interface{})
 	for i := range containers {
-		cont := &containers[i]
+		cont := containers[i]
 
 		name := cont.Labels[clab.NodeNameLabel]
 		if node, ok := c.Nodes[name]; ok && (len(node.Config().Exec) > 0) {
 			rt := node.GetRuntime()
 			contName := strings.TrimLeft(cont.Names[0], "/")
-			if execJSONResult[contName], err = execCmds(ctx, *cont, rt, node.Config().Exec, format); err != nil {
+			if execJSONResult[contName], err = execCmds(ctx, cont, rt, node.Config().Exec, format); err != nil {
 				log.Errorf("Failed to exec commands for node %s", name)
 			}
 		}
