9f62a41d6b63f042ad79a44428b2fa58947a66cd
Roman Dodin <dodin.roman@gmail.com>
2022-07-21 09:16:54 +0200

    swap openfile with create

--- types/types.go
+++ types/types.go
@@ -126,7 +126,6 @@ type HostRequirements struct {
 // GenerateConfig generates configuration for the nodes
 // out of the template based on the node configuration and saves the result to dst
 func (node *NodeConfig) GenerateConfig(dst, templ string) error {
-
 	// If the config file is already present in the node dir
 	// we do not regenerate the config unless EnforceStartupConfig is explicitly set to true and startup-config points to a file
 	// this will persist the changes that users make to a running config when booted from some startup config
@@ -136,23 +135,30 @@ func (node *NodeConfig) GenerateConfig(dst, templ string) error {
 	} else if node.EnforceStartupConfig {
 		log.Infof("Startup config for '%s' node enforced: '%s'", node.ShortName, dst)
 	}
+
 	log.Debugf("generating config for node %s from file %s", node.ShortName, node.StartupConfig)
+
 	tpl, err := template.New(filepath.Base(node.StartupConfig)).Parse(templ)
 	if err != nil {
 		return err
 	}
+
 	dstBytes := new(bytes.Buffer)
+
 	err = tpl.Execute(dstBytes, node)
 	if err != nil {
 		return err
 	}
 	log.Debugf("node '%s' generated config: %s", node.ShortName, dstBytes.String())
-	f, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666) // skipcq: GSC-G302
+
+	f, err := os.Create(dst)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
+
 	_, err = f.Write(dstBytes.Bytes())
+
 	return err
 }
 
