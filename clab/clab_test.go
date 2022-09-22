f2b75cd945455c64eca25d955a3417aecaa5d2d1
steiler <github@vahlenkamp.net>
2022-09-22 15:19:50 +0200

    adding DependencyManager tests

--- clab/clab_test.go
+++ clab/clab_test.go
@@ -5,12 +5,14 @@
 package clab
 
 import (
+	"context"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/srl-labs/containerlab/mocks"
 	"github.com/srl-labs/containerlab/nodes"
 	_ "github.com/srl-labs/containerlab/nodes/all"
+	"github.com/srl-labs/containerlab/runtime"
 	_ "github.com/srl-labs/containerlab/runtime/all"
 	"github.com/srl-labs/containerlab/types"
 )
@@ -66,6 +68,7 @@ func getNodeMap(mockCtrl *gomock.Controller) map[string]nodes.Node {
 		&types.NodeConfig{
 			Image:     "alpine:3",
 			ShortName: "node2",
+			WaitFor:   []string{"node1"},
 		},
 	).AnyTimes()
 
@@ -76,6 +79,7 @@ func getNodeMap(mockCtrl *gomock.Controller) map[string]nodes.Node {
 			Image:       "alpine:3",
 			NetworkMode: "container:node2",
 			ShortName:   "node3",
+			WaitFor:     []string{"node1", "node2"},
 		},
 	).AnyTimes()
 
@@ -86,16 +90,18 @@ func getNodeMap(mockCtrl *gomock.Controller) map[string]nodes.Node {
 			Image:           "alpine:3",
 			MgmtIPv4Address: "172.10.10.1",
 			ShortName:       "node4",
+			NetworkMode:     "container:foobar",
 		},
 	).AnyTimes()
 
-	// instantiate Mock Node 4
+	// instantiate Mock Node 5
 	mockNode5 := mocks.NewMockNode(mockCtrl)
 	mockNode5.EXPECT().Config().Return(
 		&types.NodeConfig{
 			Image:           "alpine:3",
 			MgmtIPv4Address: "172.10.10.2",
 			ShortName:       "node5",
+			WaitFor:         []string{"node3", "node4"},
 		},
 	).AnyTimes()
 
@@ -110,4 +116,100 @@ func getNodeMap(mockCtrl *gomock.Controller) map[string]nodes.Node {
 	}
 
 	return nodeMap
+}
+
+func Test_createWaitForDependency(t *testing.T) {
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// instantiate a dependencyManager mock
+	dm := mocks.NewMockDependencyManager(mockCtrl)
+
+	// retrieve a map of nodes
+	nodeMap := getNodeMap(mockCtrl)
+
+	dm.EXPECT().AddDependency("node1", "node2")
+	dm.EXPECT().AddDependency("node1", "node3")
+	dm.EXPECT().AddDependency("node2", "node3")
+	dm.EXPECT().AddDependency("node3", "node5")
+	dm.EXPECT().AddDependency("node4", "node5")
+
+	err := createWaitForDependency(nodeMap, dm)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_WaitForExternalNodeDependencies_OK(t *testing.T) {
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// init a ContainerRuntime mock
+	crMock := mocks.NewMockContainerRuntime(mockCtrl)
+
+	// context parameter
+	ctx := context.TODO()
+
+	counter := 0
+	counterMax := 3
+	// setup the container runtime mock
+	crMock.EXPECT().GetContainerStatus(ctx, "foobar").DoAndReturn(
+		func(_ context.Context, _ string) runtime.ContainerStatus {
+			counter++
+			if counter >= counterMax {
+				return runtime.Running
+			}
+			return runtime.Stopped
+
+		},
+	).Times(counterMax)
+
+	// create a barebone CLab struct
+	c := CLab{
+		Nodes:         getNodeMap(mockCtrl),
+		globalRuntime: "mock",
+		Runtimes: map[string]runtime.ContainerRuntime{
+			"mock": crMock,
+		},
+	}
+
+	// run the check
+	c.WaitForExternalNodeDependencies(ctx, "node4")
+
+	// check that the function was called "counterMax" times
+	if counter != counterMax {
+		t.Errorf("expected %q calls to runtime for status. Seen just %q", counterMax, counter)
+	}
+}
+
+func Test_WaitForExternalNodeDependencies_NoContainerNetworkMode(t *testing.T) {
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// create a barebone CLab struct
+	c := CLab{
+		Nodes: getNodeMap(mockCtrl),
+	}
+
+	// run the check with a node that has no "network-mode: container:<CONTAINERNAME>"
+	c.WaitForExternalNodeDependencies(context.TODO(), "node5")
+	// should simply and quickly return
+}
+
+func Test_WaitForExternalNodeDependencies_NodeNonExisting(t *testing.T) {
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// create a barebone CLab struct
+	c := CLab{
+		Nodes: getNodeMap(mockCtrl),
+	}
+
+	// run the check with a node that has no "network-mode: container:<CONTAINERNAME>"
+	c.WaitForExternalNodeDependencies(context.TODO(), "NonExistingNode")
+	// should simply and quickly return
 }
