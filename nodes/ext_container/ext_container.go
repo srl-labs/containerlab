406db1df7191c86971c6218f20b85abc77d4d677
steiler <github@vahlenkamp.net>
2022-11-22 14:45:09 +0100

    adjust to nodes.DefaultNode

--- nodes/ext_container/ext_container.go
+++ nodes/ext_container/ext_container.go
@@ -23,12 +23,11 @@ func init() {
 }
 
 type extcont struct {
-	cfg     *types.NodeConfig
-	runtime runtime.ContainerRuntime
+	nodes.DefaultNode
 }
 
 func (s *extcont) Init(cfg *types.NodeConfig, opts ...nodes.NodeOption) error {
-	s.cfg = cfg
+	s.Cfg = cfg
 	for _, o := range opts {
 		o(s)
 	}
@@ -36,28 +35,19 @@ func (s *extcont) Init(cfg *types.NodeConfig, opts ...nodes.NodeOption) error {
 }
 func (e *extcont) Deploy(ctx context.Context) error {
 	// check for the external dependency to be running
-	err := runtime.TimeoutWaitForContainerRunning(ctx, e.runtime, e.cfg.ShortName, e.cfg.ShortName)
+	err := runtime.TimeoutWaitForContainerRunning(ctx, e.Runtime, e.Cfg.ShortName, e.Cfg.ShortName)
 	if err != nil {
 		return err
 	}
 
 	// request nspath from runtime
-	nspath, err := e.runtime.GetNSPath(ctx, e.cfg.ShortName)
+	nspath, err := e.Runtime.GetNSPath(ctx, e.Cfg.ShortName)
 	if err != nil {
 		return errors.Wrap(err, "reading external container namespace path")
 	}
 	// set nspath in node config
-	e.cfg.NSPath = nspath
+	e.Cfg.NSPath = nspath
 	// reflect ste nodes status as created
-	e.cfg.DeploymentStatus = "created"
+	e.Cfg.DeploymentStatus = "created"
 	return nil
 }
-func (e *extcont) Config() *types.NodeConfig                                   { return e.cfg }
-func (e *extcont) PreDeploy(_, _, _ string) error                              { return nil }
-func (e *extcont) PostDeploy(_ context.Context, _ map[string]nodes.Node) error { return nil }
-func (e *extcont) WithMgmtNet(*types.MgmtNet)                                  {}
-func (e *extcont) WithRuntime(r runtime.ContainerRuntime)                      { e.runtime = r }
-func (e *extcont) GetRuntime() runtime.ContainerRuntime                        { return e.runtime }
-func (e *extcont) SaveConfig(_ context.Context) error                          { return nil }
-func (e *extcont) GetImages() map[string]string                                { return map[string]string{} }
-func (e *extcont) Delete(_ context.Context) error                              { return nil }
