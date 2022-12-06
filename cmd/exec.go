be75f8e0428a031cb3b1abf27bc575b6d861c1a9
steiler <github@vahlenkamp.net>
2022-12-06 11:00:47 +0100

    update

--- cmd/exec.go
+++ cmd/exec.go
@@ -62,7 +62,7 @@ var execCmd = &cobra.Command{
 
 		filters := []*types.GenericFilter{{FilterType: "label", Match: name, Field: "containerlab", Operator: "="}}
 		filters = append(filters, types.FilterFromLabelStrings(labels)...)
-		containers, err := c.ListContainers(ctx, filters)
+		containers, err := c.ListContainersFilter(ctx, filters)
 		if err != nil {
 			return err
 		}
