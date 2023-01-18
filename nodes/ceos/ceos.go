70a36cadc25f079541dafdb5b1f684005aec0391
Roman Dodin <dodin.roman@gmail.com>
2023-01-18 09:34:29 +0100

    fix ceos name check regexp

--- nodes/ceos/ceos.go
+++ nodes/ceos/ceos.go
@@ -276,8 +276,8 @@ func (n *ceos) ceosPostDeploy(_ context.Context) error {
 // CheckInterfaceName checks if a name of the interface referenced in the topology file correct.
 func (n *ceos) CheckInterfaceName() error {
 	// allow eth and et interfaces
-	// https://regex101.com/r/umQW5Z/1
-	ifRe := regexp.MustCompile(`eth[1-9]+$|et[1-9]+$`)
+	// https://regex101.com/r/umQW5Z/2
+	ifRe := regexp.MustCompile(`eth[1-9][\w\.]*$|et[1-9][\w\.]*$`)
 	for _, e := range n.Config().Endpoints {
 		if !ifRe.MatchString(e.EndpointName) {
 			return fmt.Errorf("arista cEOS node %q has an interface named %q which doesn't match the required pattern. Interfaces should be named as ethX or etX, where X is >1", n.Cfg.ShortName, e.EndpointName)
