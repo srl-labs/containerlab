d7a850cd124d164c75f48083f448cedb78fa222a
Roman Dodin <dodin.roman@gmail.com>
2023-02-16 13:14:06 +0100

    debug flag comments

--- clab/clab.go
+++ clab/clab.go
@@ -52,7 +52,7 @@ func WithTimeout(dur time.Duration) ClabOption {
 	}
 }
 
-// WithDebug set debug mode
+// WithDebug sets debug mode
 func WithDebug(debug bool) ClabOption {
 	return func(c *CLab) error {
 		c.Config.Debug = debug
--- clab/config.go
+++ clab/config.go
@@ -46,7 +46,8 @@ type Config struct {
 	Prefix   *string         `json:"prefix,omitempty"`
 	Mgmt     *types.MgmtNet  `json:"mgmt,omitempty"`
 	Topology *types.Topology `json:"topology,omitempty"`
-	// stores the debug flag
+	// the debug flag value as passed via cli
+	// may be used by other packages to enable debug logging
 	Debug bool `json:"debug"`
 }
 
