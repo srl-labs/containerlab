ab40d7cfec5891c56cd229b4c508eb15310ad96a
Roman Dodin <dodin.roman@gmail.com>
2023-07-29 10:59:47 +0300

    unembed set function

--- cmd/tools_netem.go
+++ cmd/tools_netem.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Nokia
+// Copyright 2023 Nokia
 // Licensed under the BSD 3-Clause License.
 // SPDX-License-Identifier: BSD-3-Clause
 
@@ -28,90 +28,95 @@ var (
 
 func init() {
 	toolsCmd.AddCommand(netemCmd)
+
 	netemCmd.AddCommand(netemSetCmd)
-	netemSetCmd.Flags().StringVarP(&NetemNode, "node", "", "", "node to apply qdisc to")
-	netemSetCmd.Flags().StringVarP(&NetemInterface, "interface", "", "", "interface to apply qdsic to")
+	netemSetCmd.Flags().StringVarP(&NetemNode, "node", "", "", "node to apply impairment to")
+	netemSetCmd.Flags().StringVarP(&NetemInterface, "interface", "", "", "interface to apply impairment to")
 	netemSetCmd.Flags().StringVarP(&NetemDelay, "delay", "", "0ms", "link receive delay")
 	netemSetCmd.Flags().StringVarP(&NetemJitter, "jitter", "", "0ms", "link receive jitter")
 	netemSetCmd.Flags().Float64VarP(&NetemLoss, "loss", "", 0, "link receive loss (0 >= rate => 100)")
 	netemSetCmd.Flags().Uint64VarP(&NetemRate, "rate", "", 0, "link receive rate in kbit")
+
 	netemSetCmd.MarkFlagRequired("node")
 	netemSetCmd.MarkFlagRequired("interface")
 }
 
 var netemCmd = &cobra.Command{
 	Use:   "netem",
-	Short: "netem operations",
+	Short: "link impairment operations",
 }
 
 var netemSetCmd = &cobra.Command{
 	Use:   "set",
 	Short: "set operation",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE:  netemSetFn,
+}
 
-		// Parse Delay and Jitter to become Durations
-		delayDur, err := time.ParseDuration(NetemDelay)
-		if err != nil {
-			return err
-		}
-		jitterDur, err := time.ParseDuration(NetemJitter)
-		if err != nil {
-			return err
-		}
+func netemSetFn(cmd *cobra.Command, args []string) error {
+	// Parse Delay and Jitter to become Durations
+	delayDur, err := time.ParseDuration(NetemDelay)
+	if err != nil {
+		return err
+	}
+	jitterDur, err := time.ParseDuration(NetemJitter)
+	if err != nil {
+		return err
+	}
 
-		// Get the runtime initializer.
-		_, rinit, err := clab.RuntimeInitializer(rt)
-		if err != nil {
-			return err
-		}
+	// Get the runtime initializer.
+	_, rinit, err := clab.RuntimeInitializer(rt)
+	if err != nil {
+		return err
+	}
 
-		// init the runtime
-		rt := rinit()
+	// init the runtime
+	rt := rinit()
 
-		// init runtime with timeout
-		err = rt.Init(
-			runtime.WithConfig(
-				&runtime.RuntimeConfig{
-					Timeout: timeout,
-				},
-			),
-		)
-		if err != nil {
-			return err
-		}
+	// init runtime with timeout
+	err = rt.Init(
+		runtime.WithConfig(
+			&runtime.RuntimeConfig{
+				Timeout: timeout,
+			},
+		),
+	)
+	if err != nil {
+		return err
+	}
 
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-		// retrieve the containers NSPath
-		nodeNsPath, err := rt.GetNSPath(ctx, NetemNode)
-		if err != nil {
-			return err
-		}
+	// retrieve the containers NSPath
+	nodeNsPath, err := rt.GetNSPath(ctx, NetemNode)
+	if err != nil {
+		return err
+	}
 
-		// get namespace handle
-		nshandle, err := netns.GetFromPath(nodeNsPath)
-		if err != nil {
-			return err
-		}
-		// get netlink handle for the namespace
-		nlHandle, err := netlink.NewHandleAt(nshandle)
-		if err != nil {
-			return err
-		}
-		// get the link by name from the namespace
-		nlLink, err := nlHandle.LinkByName(NetemInterface)
-		if err != nil {
-			return err
-		}
+	// get namespace handle
+	nshandle, err := netns.GetFromPath(nodeNsPath)
+	if err != nil {
+		return err
+	}
+	// get netlink handle for the namespace
+	nlHandle, err := netlink.NewHandleAt(nshandle)
+	if err != nil {
+		return err
+	}
+	// get the link by name from the namespace
+	nlLink, err := nlHandle.LinkByName(NetemInterface)
+	if err != nil {
+		return err
+	}
 
-		// finally set the netem parameters
-		nsFd := int(nshandle)
-		err = utils.SetDelayJitterLoss(NetemNode, nsFd, nlLink, delayDur, jitterDur, NetemLoss, NetemRate)
-		if err != nil {
-			return err
-		}
-		log.Info("Successfull")
-		return nil
-	},
+	// finally set the netem parameters
+	nsFd := int(nshandle)
+	err = utils.SetDelayJitterLoss(NetemNode, nsFd, nlLink, delayDur, jitterDur, NetemLoss, NetemRate)
+	if err != nil {
+		return err
+	}
+
+	log.Info("Successful")
+
+	return nil
 }
