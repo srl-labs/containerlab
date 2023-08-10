ef0085546397e7e8932fb3353a7b4fb4b8efdac7
ubaumann <github@m.ubaumann.ch>
2023-08-10 14:55:40 +0200

    Pin SRL in clos02

--- lab-examples/clos02/setup.sh
+++ lab-examples/clos02/setup.sh
@@ -3,7 +3,7 @@
 CFG_DIR=./configs
 
 configure_SRL() {
-  OUT=$(gnmic -a clab-clos02-$1 --timeout 30s -u admin -p $PASSWORD -e json_ietf --skip-verify set --update-path / --update-file $CFG_DIR/$1.yaml 2>&1)
+  OUT=$(gnmic -a clab-clos02-$1 --timeout 30s -u admin -p NokiaSrl1! -e json_ietf --skip-verify set --update-path / --update-file $CFG_DIR/$1.yaml 2>&1)
   echo $OUT | grep -q -e '\"operation\": \"UPDATE\"'
   if [ $? -eq 0 ]; then
     docker exec clab-clos02-$1 sr_cli "save startup" > /dev/null
@@ -22,10 +22,6 @@ echo
 PIDS=""
 NE=("leaf1" "leaf2" "leaf3" "leaf4" "spine1" "spine2" "spine3" "spine4" "superspine1" "superspine2")
 CLIENT=("client1" "client2" "client3" "client4")
-
-echo Enter SR Linux password:
-read -s PASSWORD
-echo
 
 for VARIANT in ${NE[@]}; do
   ( configure_SRL $VARIANT ) &
