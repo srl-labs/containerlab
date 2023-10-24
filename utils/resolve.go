aecaf56023efdf90254b99a85847bedec6086aa9
Roman Dodin <dodin.roman@gmail.com>
2023-10-25 00:48:35 +0300

    use slices to collect dns servers

--- utils/resolve.go
+++ utils/resolve.go
@@ -7,13 +7,17 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/exp/slices"
 )
 
 // ExtractDNSServersFromResolvConf extracts IP addresses
 // of the DNS servers from the resolv.conf-formatted files passed in filenames list.
 // Returns a list of IP addresses of the DNS servers.
 func ExtractDNSServersFromResolvConf(filesys fs.FS, filenames []string) ([]string, error) {
-	DNSServersMap := map[string]struct{}{}
+	// list of DNS servers up to 10 elements
+	// since realistically there should be no more than 3
+	// DNS servers, we leave some room for duplicates
+	DNSServers := make([]string, 0, 10)
 
 	for _, filename := range filenames {
 		readFile, err := filesys.Open(filename)
@@ -39,27 +43,23 @@ func ExtractDNSServersFromResolvConf(filesys fs.FS, filenames []string) ([]strin
 					continue
 				}
 
-				DNSServersMap[ip.String()] = struct{}{}
+				DNSServers = append(DNSServers, ip.String())
 			}
 		}
 
 		readFile.Close()
 	}
 
-	if len(DNSServersMap) == 0 {
+	if len(DNSServers) == 0 {
 		return nil, nil
 	}
 
-	DNSServers := make([]string, 0, len(DNSServersMap))
-	var count int
-	for k := range DNSServersMap {
-		if count == 3 {
-			// keep only the first three DNS servers
-			// since C DNS resolver can't handle more
-			break
-		}
-		DNSServers = append(DNSServers, k)
-		count++
+	// remove duplicates
+	slices.Sort(DNSServers)
+	DNSServers = slices.Compact(DNSServers)
+
+	if len(DNSServers) > 3 {
+		DNSServers = DNSServers[:3]
 	}
 
 	return DNSServers, nil
