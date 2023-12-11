cd7b0df1b5953c47cf2540dcef219fd568714925
akielaries <akiel@akiel.org>
2023-12-11 13:57:09 -0700

    removing vr- prefix in `vr-vjunosswitch.go`

--- nodes/vr_vjunosswitch/vr-vjunosswitch.go
+++ nodes/vr_vjunosswitch/vr-vjunosswitch.go
@@ -17,7 +17,7 @@ import (
 )
 
 var (
-	kindnames          = []string{"juniper_vjunosswitch", "vr-vjunosswitch", "vr-juniper_vjunosswitch"}
+	kindnames          = []string{"juniper_vjunosswitch"}
 	defaultCredentials = nodes.NewCredentials("admin", "admin@123")
 )
 
