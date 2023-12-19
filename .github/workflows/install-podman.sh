f811652d1e07e2cea57fc02de61c88ff92cb1805
Roman Dodin <dodin.roman@gmail.com>
2023-12-19 23:44:03 +0100

    bump artifacts v3->v4 (#1792)
    * artifacts v3->v4
    
    * refactor upload/download artifacts for v4
    
    * added matrix vars
    
    * downgrade podman to match kubic release?
    
    * replace doesn't exist
    
    * bring back recent podman
    
    * added runtime to artifacts names
    
    * trying kubic from podman install docs
    
    * endless trying
    
    * dear yaml
    
    * distinct names for vxlan logs
    
    * add timeout
    
    * use outputs for cross-wf arg pass
    
    * use needs
    
    * carve out clab build step
    
    * go ver env var fix
    
    * carve out smoke tests
    
    * carve out srl tests
    
    * added file changes dep
    
    * download all coverage artifacts
    
    * added go mod cache for static check step

---
+++ .github/workflows/install-podman.sh
@@ -0,0 +1,14 @@
+sudo apt purge -y podman
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL "https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key" \
+    | gpg --dearmor \
+    | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
+    | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
+
+sudo apt-get update -qq
+sudo apt-get -qq -y install podman
+sudo systemctl start podman
