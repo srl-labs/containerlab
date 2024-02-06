9f46409a402b385ba0266459ba63dcc2e9459790
Marko Zagožen <marko.zagozen@gmail.com>
2024-02-06 16:32:51 +0100

    Add "auto" option to ipv4-subnet and ipv6-subnet
    If we do not care about the assigned IPv4/IPv6 subnet for the container
    network, we let the container runtime allocate one for us. This
    guarantees to avoid a collision of the hardcoded range (172.20.20.0/24
    and 2001:172:20:20::/64) with any existing container networks.
    
    This just works for the podman runtime. For the docker runtime we
    compute the IPv6 subnet manually because it can only pick addresses from
    the configured (global) subnet.

--- clab/config/utils.go
+++ clab/config/utils.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"net/netip"
 	"path/filepath"
@@ -269,6 +271,21 @@ func ipFarEnd(in netip.Prefix) netip.Prefix {
 		return netip.Prefix{}
 	}
 	return netip.PrefixFrom(n, in.Bits())
+}
+
+// GenerateIPv6ULASubnet creates a random /64 IPv6 subnet from the ULA (Unique Local Address) range
+func GenerateIPv6ULASubnet() (string, error) {
+	ula := "fd00:"
+	for i := 0; i < 3; i++ {
+		// Generate a random 16-bit hex field
+		bytes := make([]byte, 2)
+		if _, err := rand.Read(bytes); err != nil {
+			return "", err
+		}
+		ula += hex.EncodeToString(bytes) + ":"
+	}
+	ula += ":/64"
+	return ula, nil
 }
 
 // GetTemplateNamesInDirs returns a list of template file names found in a list of dir `paths`
