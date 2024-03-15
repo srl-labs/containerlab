d6425592ad19849ddbf91d79508c8c8534f69ed0
Roman Dodin <dodin.roman@gmail.com>
2024-03-15 10:16:36 +0200

    augment func comment

--- clab/file.go
+++ clab/file.go
@@ -27,7 +27,8 @@ const (
 	varFileSuffix = "_vars"
 )
 
-// LoadTopology parses the topology file into c.Conf structure
+// LoadTopologyFromFile loads a topology by the topo file path
+// parses the topology file into c.Conf structure
 // as well as populates the TopoFile structure with the topology file related information.
 func (c *CLab) LoadTopologyFromFile(topo, varsFile string) error {
 	var err error
