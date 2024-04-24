f725f908e8f395b08ae8275950382c9888578202
Roman Dodin <dodin.roman@gmail.com>
2024-04-24 11:47:37 +0300

    use distro detection script and install funcs for debian and ubuntu

--- utils/debian-setup.sh
+++ utils/debian-setup.sh
@@ -1,19 +1,74 @@
+DISTRO_TYPE=""
+
+function check_os {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        if [ "$ID" = "debian" ]; then
+            ${DISTRO_TYPE}="debian"
+        elif [ "$ID" = "ubuntu" ]; then
+            ${DISTRO_TYPE}="ubuntu"
+        else
+            echo "This is not Debian or Ubuntu"
+        fi
+    else
+        echo "Cannot determine the operating system"
+    fi
+}
+
 function install-docker {
-    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+    # check OS to determine how to install docker
+    check_os
+
+    if [ "${DISTRO_TYPE}" = "debian" ]; then
+        install-docker-debian
+    elif [ "${DISTRO_TYPE}" = "ubuntu" ]; then
+        install-docker-ubuntu
+    else
+        echo "Cannot determine the operating system"
+        exit 1
+    fi
+}
+
+function install-docker-debian {
+    # using instructions from:
+    # https://docs.docker.com/engine/install/debian/#install-using-the-repository
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
     # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl gnupg
+    sudo apt-get update -y
+    sudo apt-get install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
     echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
+    sudo apt-get update -y
+
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+function install-docker-ubuntu {
+    # using instructions from:
+    # https://docs.docker.com/engine/install/debian/#install-using-the-repository
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
+
+    # Add Docker's official GPG key:
+    sudo apt-get update -y
+    sudo apt-get install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update -y
 
     sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
@@ -37,7 +92,7 @@ function install-containerlab {
     echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" | \
     sudo tee -a /etc/apt/sources.list.d/netdevops.list
 
-    sudo apt update && sudo apt install containerlab
+    sudo apt update && sudo apt install -y containerlab
 }
 
 function all {
