412caec01b138c439945c55564a3b65016ae176c
Roman Dodin <dodin.roman@gmail.com>
2024-06-07 16:58:43 +0300

    add theme

--- .github/containers/codespaces/.devcontainer/zsh/install-zsh-plugins.sh
+++ .github/containers/codespaces/.devcontainer/zsh/install-zsh-plugins.sh
@@ -1,6 +1,9 @@
 # atuin
 bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
 
+# theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # zsh-autosuggestions and autocompletions
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
