a1854e136a3528d801fd789424cabb85f12cae66
Kaelem <62122480+kaelemc@users.noreply.github.com>
2024-07-09 20:57:27 +1200

    Add interface aliasing feature.

--- nodes/vr_cat9kv/vr-cat9kv.go
+++ nodes/vr_cat9kv/vr-cat9kv.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"fmt"
 	"path"
+	"regexp"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/srl-labs/containerlab/netconf"
@@ -19,6 +20,10 @@ import (
 var (
 	kindnames          = []string{"cisco_cat9kv", "cisco_c9000v", "vr-cat9kv", "vr-c9000v"}
 	defaultCredentials = nodes.NewCredentials("admin", "admin")
+
+	InterfaceRegexp = regexp.MustCompile(`(?:Gi|GigabitEthernet)\s?1/0/(?P<port>\d+)$`)
+	InterfaceOffset = 1
+	InterfaceHelp   = "Gi1/0/X or GigabitEthernet1/0/X (where X >= 1) or ethX (where X >= 1)"
 )
 
 const (
@@ -31,17 +36,17 @@ const (
 // Register registers the node in the NodeRegistry.
 func Register(r *nodes.NodeRegistry) {
 	r.Register(kindnames, func() nodes.Node {
-		return new(vrcat9kv)
+		return new(vrCat9kv)
 	}, defaultCredentials)
 }
 
-type vrcat9kv struct {
-	nodes.DefaultNode
+type vrCat9kv struct {
+	nodes.VRNode
 }
 
-func (n *vrcat9kv) Init(cfg *types.NodeConfig, opts ...nodes.NodeOption) error {
-	// Init DefaultNode
-	n.DefaultNode = *nodes.NewDefaultNode(n)
+func (n *vrCat9kv) Init(cfg *types.NodeConfig, opts ...nodes.NodeOption) error {
+	// Init VRNode
+	n.VRNode = *nodes.NewVRNode(n)
 	// set virtualization requirement
 	n.HostRequirements.VirtRequired = true
 
@@ -72,10 +77,14 @@ func (n *vrcat9kv) Init(cfg *types.NodeConfig, opts ...nodes.NodeOption) error {
 	n.Cfg.Cmd = fmt.Sprintf("--username %s --password %s --hostname %s --connection-mode %s --vcpu %s --ram %s --trace",
 		n.Cfg.Env["USERNAME"], n.Cfg.Env["PASSWORD"], n.Cfg.ShortName, n.Cfg.Env["CONNECTION_MODE"], n.Cfg.Env["VCPU"], n.Cfg.Env["RAM"])
 
+	n.InterfaceRegexp = InterfaceRegexp
+	n.InterfaceOffset = InterfaceOffset
+	n.InterfaceHelp = InterfaceHelp
+
 	return nil
 }
 
-func (s *vrcat9kv) PreDeploy(_ context.Context, params *nodes.PreDeployParams) error {
+func (s *vrCat9kv) PreDeploy(_ context.Context, params *nodes.PreDeployParams) error {
 	utils.CreateDirectory(s.Cfg.LabDir, 0777)
 	_, err := s.LoadOrGenerateCertificate(params.Cert, params.TopologyName)
 	if err != nil {
@@ -84,7 +93,7 @@ func (s *vrcat9kv) PreDeploy(_ context.Context, params *nodes.PreDeployParams) e
 	return nodes.LoadStartupConfigFileVr(s, configDirName, startupCfgFName)
 }
 
-func (n *vrcat9kv) SaveConfig(_ context.Context) error {
+func (n *vrCat9kv) SaveConfig(_ context.Context) error {
 	err := netconf.SaveConfig(n.Cfg.LongName,
 		defaultCredentials.GetUsername(),
 		defaultCredentials.GetPassword(),
@@ -96,9 +105,4 @@ func (n *vrcat9kv) SaveConfig(_ context.Context) error {
 
 	log.Infof("saved %s running configuration to startup configuration file\n", n.Cfg.ShortName)
 	return nil
-}
-
-// CheckInterfaceName checks if a name of the interface referenced in the topology file correct.
-func (n *vrcat9kv) CheckInterfaceName() error {
-	return nodes.GenericVMInterfaceCheck(n.Cfg.ShortName, n.Endpoints)
 }
