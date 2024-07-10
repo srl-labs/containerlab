12ac8431a6186a31a5b99ada05170370fb090a71
kaelemc <62122480+kaelemc@users.noreply.github.com>
2024-07-10 22:02:10 +1200

    add -y flag to rhel install commands

--- utils/quick-setup.sh
+++ utils/quick-setup.sh
@@ -7,7 +7,7 @@ function check_os {
             DISTRO_TYPE="debian"
         elif [ "$ID" = "ubuntu" ]; then
             DISTRO_TYPE="ubuntu"
-        elif [ "$ID" = "rocky" ]; then
+        elif [[ "$ID" = "rocky" || "$ID" = "rhel" || "$ID" = "centos"  || "$ID" = "fedora" ]]; then
             DISTRO_TYPE="rhel"
         else
             echo "This is not Debian or Ubuntu"
@@ -78,7 +78,7 @@ function install-docker-ubuntu {
 function install-docker-rhel {
     # using instructions from:
     # https://docs.docker.com/engine/install/rhel/#install-using-the-repository
-    sudo yum remove docker \
+    sudo yum remove -y docker \
                   docker-client \
                   docker-client-latest \
                   docker-common \
@@ -90,9 +90,9 @@ function install-docker-rhel {
                   runc
 
     sudo yum install -y yum-utils
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+    sudo yum-config-manager -y --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
-    sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # diverges from the instructions. This means docker daemon starts on each boot.
     sudo systemctl enable --now docker
@@ -126,16 +126,13 @@ function install-gh-cli-debian {
 }
 
 function install-gh-cli-rhel {
-    sudo dnf install 'dnf-command(config-manager)'
-    sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+    sudo dnf install -y 'dnf-command(config-manager)'
+    sudo dnf config-manager -y --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+    sudo dnf install git -y
     sudo dnf install -y gh --repo gh-cli
 }
 
 function install-containerlab {
-    # echo "deb [trusted=yes] https://netdevops.fury.site/apt/ /" | \
-    # sudo tee -a /etc/apt/sources.list.d/netdevops.list
-
-    # sudo apt update && sudo apt install -y containerlab
     sudo bash -c "$(curl -sL https://get.containerlab.dev)"
 }
 
