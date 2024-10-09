3dea806bb6759524b0200a549dc85dd115dcd7e8
Roman Dodin <dodin.roman@gmail.com>
2024-10-09 12:46:37 +0300

    fix different cli package version

--- utils/quick-setup.sh
+++ utils/quick-setup.sh
@@ -106,8 +106,9 @@ function install-docker-rhel {
     sudo yum-config-manager -y --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
     DOCKER_PKG_NAME=$(yum list docker-ce --showduplicates | awk '{ print $2 }' | grep ${DOCKER_VERSION} | head -n 1)
+    DOCKER_CLI_PKG_NAME=$(yum list docker-ce-cli --showduplicates | awk '{ print $2 }' | grep ${DOCKER_VERSION} | head -n 1)
 
-    sudo yum install -y docker-ce-${DOCKER_PKG_NAME} docker-ce-cli-${DOCKER_PKG_NAME} containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo yum install -y docker-ce-${DOCKER_PKG_NAME} docker-ce-cli-${DOCKER_CLI_PKG_NAME} containerd.io docker-buildx-plugin docker-compose-plugin
 
     # diverges from the instructions. This means docker daemon starts on each boot.
     sudo systemctl enable --now docker
@@ -131,8 +132,9 @@ function install-docker-fedora {
     sudo dnf config-manager -y --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
     DOCKER_PKG_NAME=$(dnf list docker-ce --showduplicates | awk '{ print $2 }' | grep ${DOCKER_VERSION} | head -n 1)
+    DOCKER_CLI_PKG_NAME=$(dnf list docker-ce-cli --showduplicates | awk '{ print $2 }' | grep ${DOCKER_VERSION} | head -n 1)
 
-    sudo dnf install -y docker-ce-${DOCKER_PKG_NAME} docker-ce-cli-${DOCKER_PKG_NAME} containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo dnf install -y docker-ce-${DOCKER_PKG_NAME} docker-ce-cli-${DOCKER_CLI_PKG_NAME} containerd.io docker-buildx-plugin docker-compose-plugin
 
     # diverges from the instructions. This means docker daemon starts on each boot.
     sudo systemctl enable --now docker
