41e8ae44482f3ab2078879c0c394cf528e5e63b0
Roman Dodin <dodin.roman@gmail.com>
2024-11-30 17:41:26 +0100

    test fira code embedded and zsh term settings

--- .devcontainer/zsh/install-zsh-plugins.sh
+++ .devcontainer/zsh/install-zsh-plugins.sh
@@ -20,3 +20,16 @@ git clone --depth 1 https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.o
 /usr/bin/containerlab completion zsh > "/home/vscode/.oh-my-zsh/custom/plugins/zsh-autocomplete/Completions/_containerlab"
 # add clab alias to the completions
 sed -i 's/compdef _containerlab containerlab/compdef _containerlab containerlab clab/g' /home/vscode/.oh-my-zsh/custom/plugins/zsh-autocomplete/Completions/_containerlab
+
+###
+### FiraCode Nerd Font
+###
+
+mkdir -p ~/fonts && pushd ~/fonts
+curl -LO https://gitlab.com/rdodin/pics/-/wikis/uploads/5a7581110f60e3ac87c3fe4f8c2204cf/FiraCodeNerdFont-Bold.ttf
+curl -LO https://gitlab.com/rdodin/pics/-/wikis/uploads/8bdbb5f62cd60b567b599bb0c2673473/FiraCodeNerdFont-Light.ttf
+curl -LO https://gitlab.com/rdodin/pics/-/wikis/uploads/58c9962f71b86e098a9bd62652b47248/FiraCodeNerdFont-Medium.ttf
+curl -LO https://gitlab.com/rdodin/pics/-/wikis/uploads/58c9962f71b86e098a9bd62652b47248/FiraCodeNerdFont-Regular.ttf
+curl -LO https://gitlab.com/rdodin/pics/-/wikis/uploads/58c9962f71b86e098a9bd62652b47248/FiraCodeNerdFont-Retina.ttf
+curl -LO https://gitlab.com/rdodin/pics/-/wikis/uploads/58c9962f71b86e098a9bd62652b47248/FiraCodeNerdFont-SemiBold.ttf
+popd
