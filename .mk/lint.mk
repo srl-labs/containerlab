f8eb29b29f60a169f685774c742c0a913af92877
Carl Montanari <carl.r.montanari@gmail.com>
2025-07-11 09:27:10 -0700

    chore: repin golagncli-lint, migrate config to v2

--- .mk/lint.mk
+++ .mk/lint.mk
@@ -4,7 +4,7 @@ GOFUMPT_FLAGS := -l -w .
 GODOT_CMD := docker run --rm -it -v $(CURDIR):/work ghcr.io/hellt/godot:1.4.11
 GODOT_FLAGS := -w .
 
-GOLANGCI_CMD := docker run -it --rm -v $(CURDIR):/app -w /app golangci/golangci-lint:v1.47.2 golangci-lint
+GOLANGCI_CMD := docker run -it --rm -v $(CURDIR):/app -w /app golangci/golangci-lint:v2.2.1 golangci-lint
 GOLANGCI_FLAGS := --config ./.github/workflows/linters/.golangci.yml run -v --fix
 
 
