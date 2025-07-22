dffb07030d795ec0242647df03cfd758e5bd9c17
hellt <dodin.roman@gmail.com>
2025-07-22 21:37:02 +0200

    bring back podman v4

--- .github/workflows/install-podman.sh
+++ .github/workflows/install-podman.sh
@@ -1,45 +1,15 @@
-#!/bin/bash
-
 sudo apt purge -y podman
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL "https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key" \
+    | gpg --dearmor \
+    | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
 
-# Roman: copied from https://github.com/containers/podman/discussions/25582#discussioncomment-12803424
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
+    | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
 
-# Must be run as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root (e.g., sudo $0)"
-  exit 1
-fi
-
-# Define file paths
-PINNING_FILE="/etc/apt/preferences.d/podman-plucky.pref"
-SOURCE_LIST="/etc/apt/sources.list.d/plucky.list"
-
-# Write Plucky APT source list
-echo "Adding Plucky repo to $SOURCE_LIST..."
-echo "deb http://archive.ubuntu.com/ubuntu plucky main universe" > "$SOURCE_LIST"
-
-# Write APT pinning rules
-echo "Writing APT pinning rules to $PINNING_FILE..."
-cat <<EOF > "$PINNING_FILE"
-Package: podman buildah golang-github-containers-common crun libgpgme11t64 libgpg-error0 golang-github-containers-image catatonit conmon containers-storage
-Pin: release n=plucky
-Pin-Priority: 991
-
-Package: libsubid4 netavark passt aardvark-dns containernetworking-plugins libslirp0 slirp4netns
-Pin: release n=plucky
-Pin-Priority: 991
-
-Package: *
-Pin: release n=plucky
-Pin-Priority: 400
-EOF
-
-# Update APT cache
-echo "Updating APT package list..."
-apt update
-
-echo "Plucky pinning setup complete."
-
+sudo apt-get update -qq
 sudo apt-get -qq -y install podman
 sudo systemctl start podman
 sudo mkdir -p /var/run/netns
