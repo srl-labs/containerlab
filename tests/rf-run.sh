c6f9d6c2c5c795e4ca625942076a3218b6912166
schavezc <sergio.chavez_cardenas@nokia.com>
2025-07-22 14:07:09 -0700

    comments review & add srsim distributed test

--- tests/rf-run.sh
+++ tests/rf-run.sh
@@ -15,7 +15,6 @@ fi
 
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
-export SRSIM_LICENSE
 
 echo "Running tests with containerlab binary at $(which ${CLAB_BIN}) path and selected runtime: $1"
 
