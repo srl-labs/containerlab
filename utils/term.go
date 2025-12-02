d4749440114a0aac2c9cedf1697b0af6c6142f75
Tobias Klauser <tklauser@distanz.ch>
2025-12-02 10:08:39 +0100

    Inline single use of utils.IsTerminal
    utils.IsTerminal is a simple wrapper around golang.org/x/term.IsTerminal
    that is already called directly elsewhere in core. Inline the remaining
    caller and drop utils.IsTerminal

--- utils/term.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "golang.org/x/term"
-
-// IsTerminal returns true if the given file descriptor is a terminal.
-func IsTerminal(fd uintptr) bool {
-	return term.IsTerminal(int(fd))
-}
