2b318d4b62f4e7a5a93f7993732125065843b571
Dan Mills <evilhamsterman@users.noreply.github.com>
2026-04-23 02:46:13 -0700

    Add link IP support to VyOS and cEOS (#3158)
    As the title says, this adds support for the Link IPs feature added in
    0.70 to the VyOS and cEOS kinds
    
    ---------
    
    Co-authored-by: Roman <dodin.roman@gmail.com>

--- nodes/vyosnetworks_vyos/vyos.go
+++ nodes/vyosnetworks_vyos/vyos.go
@@ -153,11 +153,11 @@ func (n *vyos) PostDeploy(ctx context.Context, params *clabnodes.PostDeployParam
 
 	var cfgs []string
 
-	addressCmd := func(a string, p int) string {
-		log.Debug("Setting mgmt address", "address", a, "subnet", p)
+	addressCmd := func(iface, a string, p int) string {
+		log.Debug("Setting address", "interface", iface, "address", a, "subnet", p)
 		return fmt.Sprintf(
 			"set interfaces ethernet %s address %s/%d",
-			nodeCfg.MgmtIntf,
+			iface,
 			a,
 			p,
 		)
@@ -168,7 +168,7 @@ func (n *vyos) PostDeploy(ctx context.Context, params *clabnodes.PostDeployParam
 		prefix := nodeCfg.MgmtIPv4PrefixLength
 		cfgs = append(
 			cfgs,
-			addressCmd(ip, prefix),
+			addressCmd(nodeCfg.MgmtIntf, ip, prefix),
 		)
 	}
 
@@ -177,20 +177,45 @@ func (n *vyos) PostDeploy(ctx context.Context, params *clabnodes.PostDeployParam
 		prefix := nodeCfg.MgmtIPv6PrefixLength
 		cfgs = append(
 			cfgs,
-			addressCmd(ip, prefix),
+			addressCmd(nodeCfg.MgmtIntf, ip, prefix),
 		)
+	}
+
+	// configure data interfaces
+	for _, e := range n.Endpoints {
+		ifName := e.GetIfaceName()
+		// skip management interface
+		if ifName == nodeCfg.MgmtIntf {
+			continue
+		}
+
+		v4 := e.GetIPv4Addr()
+		v6 := e.GetIPv6Addr()
+
+		if !v4.IsValid() && !v6.IsValid() {
+			continue
+		}
+
+		if v4.IsValid() {
+			cfgs = append(cfgs, addressCmd(ifName, v4.Addr().String(), v4.Bits()))
+		}
+		if v6.IsValid() {
+			cfgs = append(cfgs, addressCmd(ifName, v6.Addr().String(), v6.Bits()))
+		}
 	}
 
 	if n.SSHPubKeys != nil {
 		cfgs = slices.Concat(cfgs, n.authorizedKeyCmds())
 	}
 
+	log.Debugf("VyOS PostDeploy configuration for node %s: %v", n.Cfg.ShortName, cfgs)
+
 	resp, err := cli.SendConfigs(cfgs)
 	log.Debug("CLI", "response", resp.JoinedResult())
 	if err != nil {
 		return err
 	} else if resp.Failed != nil {
-		return errors.New("failed to configure management interface")
+		return errors.New("failed to apply configuration")
 	}
 	if err := n.save(ctx, cli); err != nil {
 		return err
