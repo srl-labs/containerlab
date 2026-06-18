e1c2c220a75622d721a227b628df2dafb6afabac
Kaelem Chandra <kc@kaelem.net>
2026-06-19 01:25:43 +1200

    automatically try calc max_nics

--- nodes/vr_sros/variant_test.go
+++ nodes/vr_sros/variant_test.go
@@ -44,14 +44,26 @@ func TestBuildSrosVariant(t *testing.T) {
 					MDA:  clabtypes.MDAS{{Slot: 1, Type: "m6-10g-sfp++4-25g-sfp28"}},
 				},
 			},
-			want: "cpu=2 ram=4 chassis=ixr-r6 slot=A card=cpiom-ixr-r6 mda/1=m6-10g-sfp++4-25g-sfp28",
+			want: "cpu=2 ram=4 max_nics=10 chassis=ixr-r6 slot=A card=cpiom-ixr-r6 mda/1=m6-10g-sfp++4-25g-sfp28",
+		},
+		"env-max_nics-overrides-derived": {
+			chassis: "ixr-r6",
+			components: []*clabtypes.Component{
+				{
+					Slot: "A",
+					Type: "cpiom-ixr-r6",
+					Env:  map[string]string{"max_nics": "99"},
+					MDA:  clabtypes.MDAS{{Slot: 1, Type: "m6-10g-sfp++4-25g-sfp28"}},
+				},
+			},
+			want: "max_nics=99 chassis=ixr-r6 slot=A card=cpiom-ixr-r6 mda/1=m6-10g-sfp++4-25g-sfp28",
 		},
 		"integrated-empty-slot-defaults-to-A": {
 			chassis: "sr-1",
 			components: []*clabtypes.Component{
 				{Type: "iom-1", MDA: clabtypes.MDAS{{Slot: 1, Type: "me6-100gb-qsfp28"}}},
 			},
-			want: "chassis=sr-1 slot=A card=iom-1 mda/1=me6-100gb-qsfp28",
+			want: "max_nics=6 chassis=sr-1 slot=A card=iom-1 mda/1=me6-100gb-qsfp28",
 		},
 		"distributed-cpm-and-lc": {
 			chassis: "ixr-e",
@@ -81,7 +93,7 @@ func TestBuildSrosVariant(t *testing.T) {
 				},
 			},
 			want: "cp: chassis=sr-2s slot=A sfm=sfm-2s card=cpm-2s ___ " +
-				"lc: chassis=sr-2s slot=1 sfm=sfm-2s card=xcm-2s xiom/x1=iom-s-3.0t mda/x1/1=ms8-100gb-sfpdd+2-100gb-qsfp28",
+				"lc: max_nics=10 chassis=sr-2s slot=1 sfm=sfm-2s card=xcm-2s xiom/x1=iom-s-3.0t mda/x1/1=ms8-100gb-sfpdd+2-100gb-qsfp28",
 		},
 	}
 
@@ -96,4 +108,36 @@ func TestBuildSrosVariant(t *testing.T) {
 			assert.Equal(t, tc.want, got)
 		})
 	}
+}
+
+func TestMdaPortCount(t *testing.T) {
+	cases := map[string]int{
+		"m24-sfp++8-sfp28+2-qsfp28":      34,
+		"m6-10g-sfp++1-100g-qsfp28":      7,
+		"m48-sfp++6-qsfp28":              54,
+		"m32-qsfp28+4-qsfpdd":            36,
+		"m4-1g-tx+20-1g-sfp+6-10g-sfp+":  30,
+		"ms8-100gb-sfpdd+2-100gb-qsfp28": 10,
+		"s36-100gb-qsfp28":               36,
+		"ms24-10/100gb-sfpdd":            24,
+	}
+	for mda, want := range cases {
+		t.Run(mda, func(t *testing.T) {
+			assert.Equal(t, want, mdaPortCount(mda))
+		})
+	}
+}
+
+func TestComponentPortCountSumsDirectAndXiomMdas(t *testing.T) {
+	c := &clabtypes.Component{
+		Slot: "1",
+		Type: "xcm-2s",
+		XIOM: clabtypes.XIOMS{
+			{Slot: 1, Type: "iom-s-3.0t", MDA: clabtypes.MDAS{
+				{Slot: 1, Type: "ms18-100gb-qsfp28"},
+				{Slot: 2, Type: "ms24-10/100gb-sfpdd"},
+			}},
+		},
+	}
+	assert.Equal(t, 42, componentPortCount(c))
 }
