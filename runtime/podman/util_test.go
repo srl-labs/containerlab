bf87c756a22e7209b70fe5e01c1122a0d4c0af2a
Paul Coiner <paul.coiner@oracele.com>
2026-07-14 16:28:05 -0400

    Preserve Podman image command defaults

--- runtime/podman/util_test.go
+++ runtime/podman/util_test.go
@@ -118,3 +118,25 @@ func TestCreateContainerSpecAppliesNoneNetworkMode(t *testing.T) {
 		t.Fatalf("NetNS mode = %q, want %q", sg.NetNS.NSMode, specgen.NoNetwork)
 	}
 }
+
+func TestCreateContainerSpecPreservesImageCommandDefaults(t *testing.T) {
+	r := &PodmanRuntime{mgmt: &types.MgmtNet{Network: "clab"}}
+	cfg := &types.NodeConfig{
+		LongName:  "clab-test-node1",
+		ShortName: "node1",
+		Image:     "localhost/test:latest",
+		Labels:    map[string]string{},
+	}
+
+	sg, err := r.createContainerSpec(context.Background(), cfg)
+	if err != nil {
+		t.Fatalf("createContainerSpec returned error: %v", err)
+	}
+
+	if sg.Command != nil {
+		t.Fatalf("Command = %#v, want nil to preserve the image default", sg.Command)
+	}
+	if sg.Entrypoint != nil {
+		t.Fatalf("Entrypoint = %#v, want nil to preserve the image default", sg.Entrypoint)
+	}
+}
